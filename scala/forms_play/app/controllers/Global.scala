4978b8ec7533ef1db7738c632f9d0e68dda43cee
jmvanel <jeanmarc.vanel@gmail.com>
2014-07-30 10:37:05 +0100

    Scala forms_play (Play! framework): now user can type or paste the URI to display ( tested with: http://danbri.org/foaf.rdf#danbri with gives: http://localhost:9000/display?displayuri=http%3A%2F%2Fdanbri.org%2Ffoaf.rdf%23danbri )

--- scala/forms_play/app/controllers/Global.scala
+++ scala/forms_play/app/controllers/Global.scala
@@ -4,18 +4,29 @@ import deductions.runtime.html.TableView
 import scala.xml.Elem
 import deductions.runtime.sparql_cache.PopulateRDFCache
 
-object Global extends GlobalSettings {
+package global {
+
+object Global extends play.api.GlobalSettings {
+  var form : Elem = <p>initial value</p>
   val tv = new TableView {}
+  
   override def onStart(app: Application) {
     val uri = "http://jmvanel.free.fr/jmv.rdf#me"
     PopulateRDFCache.loadCommonVocabularies
     
-    global.Global.form = tv.htmlForm(uri)
+//    global.Global.
+    form = htmlForm(uri)
   }
+
+    def htmlForm(uri: String): scala.xml.Elem = {
+      if (uri != null && uri != "")
+        tv.htmlForm(uri)
+      else
+        <p>Enter an URI</p>
+    }
 }
 
-package global {
-  object Global {
-      var form : Elem = <p>initial value</p>
-  }
+//  object Global {
+//      var form : Elem = <p>initial value</p>
+//  }
 }
