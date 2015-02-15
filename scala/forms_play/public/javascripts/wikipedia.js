f80c52d81423ca3639f7428c0655b144bf6e6629
jmvanel <jeanmarc.vanel@gmail.com>
2015-02-15 20:03:01 +0100

    DBPediaLookup #2 : wikipedia.js : fixed errors; now we get web service results; TODO populate pulldown menu with string responses, while keeping associated URI's : line 25

--- scala/forms_play/public/javascripts/wikipedia.js
+++ scala/forms_play/public/javascripts/wikipedia.js
@@ -4,28 +4,31 @@
 // wget "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=10&QueryString=berl" --header='Accept: application/json'
 
 var urlReqPrefix = "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=10&QueryString=" ;
-function dbpedia_callback(aEvt) {
-   var response= eval( req.responseText ); // TODO not secure!
-   console.log( "response " + response );
-   inputElement.text = response.results.uri; // TODO populate pulldown menu
-};
+var req = new XMLHttpRequest();
 
 function dbpediaComplete( inputElement, callback ) {
   console.log( "dbpediaComplete " + inputElement );
-  var req = new XMLHttpRequest();
-  var word = inputElement.text ;
+  var word = inputElement.value ;
   console.log( "dbpediaComplete word " + word );
   req.open('GET', urlReqPrefix + word, true);
   req.setRequestHeader('Accept', 'application/json');
   req.onreadystatechange = callback;
   req.send(null);
+  console.log( "dbpediaComplete req sent!" );
 };
 
 function installDbpediaComplete( inputElement) {
+  function dbpedia_callback(aEvt) {
+    if (req.readyState == 4) {
+     console.log( "req.response " + req.response );
+     var response= eval( '(' + req.responseText + ')' ); // TODO eval not secure!
+     element.text = response.results.uri; // TODO populate pulldown menu with string responses, while keeping associated URI's
+    };
+  };
   console.log( "inputElement " + inputElement );
   console.log( "getElementByName(inputElement " + document.getElementsByName(inputElement) );
   var element = document.getElementsByName(inputElement)[0];
-  element.onChange = function() { dbpediaComplete( element, dbpedia_callback); };
+  element.onkeyup = function() { dbpediaComplete( element, dbpedia_callback); };
 };
 
 function test_callback(aEvt) {
