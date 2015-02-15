d30a6bf2215444875e41d4879f7173c74b0ef49e
jmvanel <jeanmarc.vanel@gmail.com>
2015-02-15 11:37:33 +0100

    DBPediaLookup #2 : debugging wikipedia.js : fixed errors reported by Sylvain; now element.onChange does not trigger

--- scala/forms_play/public/javascripts/wikipedia.js
+++ scala/forms_play/public/javascripts/wikipedia.js
@@ -3,38 +3,36 @@
 // http://stackoverflow.com/questions/148901/is-there-a-better-way-to-do-optional-function-parameters-in-javascript
 // wget "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=10&QueryString=berl" --header='Accept: application/json'
 
-var urlReqPrefix = "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=10&QueryString="
-
-function installDbpediaComplete( inputElement) {
-  // inputElement refers to attribute	<input name="
-  // on user input launch completion ??????
-  inputElement.onChange = dbpediaComplete( inputElement )
+var urlReqPrefix = "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=10&QueryString=" ;
+function dbpedia_callback(aEvt) {
+   var response= eval( req.responseText ); // TODO not secure!
+   console.log( "response " + response );
+   inputElement.text = response.results.uri; // TODO populate pulldown menu
 };
 
 function dbpediaComplete( inputElement, callback ) {
+  console.log( "dbpediaComplete " + inputElement );
   var req = new XMLHttpRequest();
-  callback = (typeof callback === "undefined") ? dbpedia_callback : callback
-  val word = inputElement.text
+  var word = inputElement.text ;
+  console.log( "dbpediaComplete word " + word );
   req.open('GET', urlReqPrefix + word, true);
-  // 'Accept: application/json'
   req.setRequestHeader('Accept', 'application/json');
   req.onreadystatechange = callback;
-
   req.send(null);
+};
 
-  function dbpedia_callback(aEvt) {
-   // decode req.responseText
-   response = eval( req.responseText ) // TODO not secure!
-   // update inputElement
-  inputElement.text = response.results.uri // TODO populate pulldown menu
-  };
+function installDbpediaComplete( inputElement) {
+  console.log( "inputElement " + inputElement );
+  console.log( "getElementByName(inputElement " + document.getElementsByName(inputElement) );
+  var element = document.getElementsByName(inputElement)[0];
+  element.onChange = function() { dbpediaComplete( element, dbpedia_callback); };
 };
 
 function test_callback(aEvt) {
   if (req.readyState == 4) {
-     if(req.status == 200)
+     if(req.status == 200) {
       dump(req.responseText);
-     else
-      dump("Erreur pendant le chargement de la page.\n");
+     } else {
+      dump("Erreur pendant le chargement de la page.\n"); }
   }
 };
