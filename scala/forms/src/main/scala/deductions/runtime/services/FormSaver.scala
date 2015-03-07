jmvanel/semantic_forms

commit b505b0b002aeacfca0778084c2c80b3dd3117bf1

Add possibility to save a triple S P O in a named graph other than S ; add some lazy declarations in InstanceLabelsInference2 to avoid NPE in project corporate_risk

scala/forms/src/main/scala/deductions/runtime/services/FormSaver.scala
@@ -16,40 +16,40 @@ import scala.util.Try
 import scala.concurrent.Future
 import org.w3.banana._
 import org.w3.banana.jena.Jena
-//import deductions.runtime.jena.RDFStoreLocalProvider
 
 object FormSaverObject extends FormSaver[Jena]
-//class Test1 { FormSaverObject.saveTriples(Map()) }
 
 class FormSaver[Rdf <: RDF]()(
     implicit ops: RDFOps[Rdf],
     sparqlOps: SparqlOps[Rdf],
     writer: RDFWriter[Rdf, Try, Turtle],
-    rdfStore: RDFStore[Rdf, Try, RDFStoreObject.DATASET]) //extends RDFStoreLocalProvider
-    //  extends TurtleWriterModule 
-    {
+    rdfStore: RDFStore[Rdf, Try, RDFStoreObject.DATASET]) {
   import ops._
   import sparqlOps._
 
   def saveTriples(map: Map[String, Seq[String]]) = {
     println("FormSaver.saveTriples")
     val uriOption = map.getOrElse("uri", Seq()).headOption
-    println("FormSaver.saveTriples " + uriOption)
+    val graphURIOption = map.getOrElse("graphURI", Seq()).headOption
+    println(s"FormSaver.saveTriples uri $uriOption, graphURI $graphURIOption")
 
     val triples = ArrayBuffer[Rdf#Triple]()
     val triplesToRemove = ArrayBuffer[Rdf#Triple]()
 
     uriOption match {
       case Some(uri0) =>
+        //        val graphURI = URLDecoder.decode(uri0, "utf-8")
         val uri = URLDecoder.decode(uri0, "utf-8")
+        val graphURI = URLDecoder.decode(graphURIOption.getOrElse(uri0), "utf-8")
         val v = map.map {
           case (prop0, obj) =>
             val prop = URLDecoder.decode(prop0, "utf-8")
             saveTriplesForProperty(uri, prop, obj, map)
         }
-        doSave(uri: String)
+        doSave(graphURI: String)
       case _ =>
     }
+
     // end of body of saveTriples
 
     def saveTriplesForProperty(uri: String, prop: String, objects: Seq[String], map: Map[String, Seq[String]]) = {
@@ -61,8 +61,6 @@ class FormSaver[Rdf <: RDF]()(
     def processChange(uri: String, prop: String, obj: String): (Boolean, String) = {
       val originalValue = map.getOrElse(
         "ORIG-" + URLEncoder.encode(prop, "utf-8"), Seq("")).headOption.getOrElse("")
-      //      val userValue = map.getOrElse(
-      //          "ORIG-" + URLEncoder.encode(prop,"utf-8"), Seq("")).headOption.getOrElse("")
       val userValue = obj
       println("processChange " + uri + " " + prop + " " + obj +
         " userValue " + userValue + " originalValue " + originalValue)
@@ -108,12 +106,12 @@ class FormSaver[Rdf <: RDF]()(
       }
     }
 
-    def doSave(uri: String) {
+    def doSave(graphURI: String) {
       import ops._
       val transaction =
         rdfStore.rw(RDFStoreObject.dataset, {
-          rdfStore.removeTriples(RDFStoreObject.dataset, makeUri(uri), triplesToRemove.toIterable)
-          rdfStore.appendToGraph(RDFStoreObject.dataset, makeUri(uri), makeGraph(triples))
+          rdfStore.removeTriples(RDFStoreObject.dataset, makeUri(graphURI), triplesToRemove.toIterable)
+          rdfStore.appendToGraph(RDFStoreObject.dataset, makeUri(graphURI), makeGraph(triples))
         }).flatMap { identity }
 
       val f = transaction.asFuture
@@ -121,12 +119,12 @@ class FormSaver[Rdf <: RDF]()(
       f onSuccess {
         case _ =>
           println("Successfully stored triples in store")
-          rdfStore.getGraph(RDFStoreObject.dataset, makeUri(uri)).asFuture.
+          rdfStore.getGraph(RDFStoreObject.dataset, makeUri(graphURI)).asFuture.
             onSuccess {
               case gr =>
                 if (triplesToRemove.size > 0 ||
                   triples.size > 0) {
-                  val graphAsString = writer.asString(gr, base = uri) getOrElse sys.error(
+                  val graphAsString = writer.asString(gr, base = graphURI) getOrElse sys.error(
                     "coudn't serialize the graph")
                   println("Graph with modifications:\n" + graphAsString)
                 }

scala/forms/src/main/scala/deductions/runtime/abstract_syntax/FormModule.scala
@@ -20,7 +20,8 @@ trait FormModule[NODE, URI <: NODE] {
   case class FormSyntax[NODE, URI <: NODE](
       val subject: NODE,
       val fields: Seq[Entry],
-      classs: URI = nullURI) {
+      classs: URI = nullURI //     , graphURI: URI = nullURI
+      ) {
     override def toString(): String = {
       s"""FormSyntax:
         subject: $subject

scala/forms/src/main/scala/deductions/runtime/abstract_syntax/FormSyntaxFactory.scala
@@ -173,9 +173,9 @@ class FormSyntaxFactory[Rdf <: RDF](val graph: Rdf#Graph, preferedLanguage: Stri
     }
 
     def addOneEntry(object_ : Rdf#Node) = {
-
+      val literalPlaceHolder = "..empty.."
       def literalEntry = new LiteralEntry(label, comment, prop, DatatypeValidator(ranges),
-        getStringOrElse(object_, "..empty.."),
+        getStringOrElse(object_, literalPlaceHolder),
         type_ = rdfh.nodeSeqToURISeq(ranges).headOption.getOrElse(nullURI))
       def resourceEntry = {
         ops.foldNode(object_)(
@@ -184,9 +184,10 @@ class FormSyntaxFactory[Rdf <: RDF](val graph: Rdf#Graph, preferedLanguage: Stri
             type_ = rdfh.nodeSeqToURISeq(ranges).headOption.getOrElse(nullURI)),
           object_ => makeBN(label, comment, prop, ResourceValidator(ranges), object_, typ = rdfh.nodeSeqToURISeq(ranges).headOption.getOrElse(nullURI)
           ),
-          object_ => new LiteralEntry(label, comment, prop, DatatypeValidator(ranges),
-            getStringOrElse(object_, "..empty.."),
-            type_ = rdfh.nodeSeqToURISeq(ranges).headOption.getOrElse(nullURI))
+          object_ => literalEntry
+        // new LiteralEntry(label, comment, prop, DatatypeValidator(ranges),
+        // getStringOrElse(object_, literalPlaceHolder ),
+        // type_ = rdfh.nodeSeqToURISeq(ranges).headOption.getOrElse(nullURI))
         )
       }
       val xsdPrefix = XSDPrefix[Rdf].prefixIri

scala/forms/src/main/scala/deductions/runtime/abstract_syntax/InstanceLabelsInference2.scala
@@ -17,8 +17,8 @@ import org.w3.banana.RDFOpsModule
 trait InstanceLabelsInference2[Rdf <: RDF] extends RDFOpsModule {
 
   import ops._
-  val foaf = FOAFPrefix[Rdf]
-  val rdfs = RDFSPrefix[Rdf]
+  lazy val foaf = FOAFPrefix[Rdf]
+  lazy val rdfs = RDFSPrefix[Rdf]
 
   def instanceLabels(list: Seq[Rdf#URI])(implicit graph: Rdf#Graph): Seq[String] = list.map(instanceLabel)
 

scala/forms/src/main/scala/deductions/runtime/html/CreationForm.scala
@@ -37,7 +37,7 @@ trait CreationForm extends RDFOpsModule
         val factory = new UnfilledFormFactory[Rdf](allNamedGraphs, preferedLanguage = lang)
         val form = factory.createFormFromClass(URI(classUri))
         println(form)
-        val htmlForm = generateHTML(form, hrefPrefix = "", editable = true, actionURI)
+        val htmlForm = generateHTML(form, hrefPrefix = "", editable = true, actionURI = actionURI)
         htmlForm
       }
     })

scala/forms/src/main/scala/deductions/runtime/html/Form2HTML.scala
@@ -16,7 +16,7 @@ trait Form2HTML[NODE, URI <: NODE] extends FormModule[NODE, URI] {
   def generateHTML(form: fm#FormSyntax[NODE, URI],
     hrefPrefix: String = "",
     editable: Boolean = false,
-    actionURI: String = "/save"): Elem = {
+    actionURI: String = "/save", graphURI: String = ""): Elem = {
 
     val htmlForm =
       <div class="form">
@@ -43,6 +43,7 @@ trait Form2HTML[NODE, URI <: NODE] extends FormModule[NODE, URI] {
           <input value="SAVE" type="submit" class="btn btn-primary btn-lg"/>
         </p>
         <input type="hidden" name="url" value={ urlEncode(form.subject) }/>
+        <input type="hidden" name="graphURI" value={ urlEncode(graphURI) }/>
         { htmlForm }
         <p class="text-right">
           <input value="SAVE" type="submit" class="btn btn-primary btn-lg pull-right"/>

scala/forms/src/main/scala/deductions/runtime/html/TableView.scala
@@ -36,11 +36,16 @@ trait TableViewModule
   val nullURI: Rdf#URI = ops.URI("")
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  /** create a form for given URI with background knowledge in RDFStoreObject.store  */
+  /**
+   * create a form for given URI with background knowledge in RDFStoreObject.store;
+   *  by default user inputs will be saved in named graph uri, except if given graphURIarg
+   */
   def htmlForm(uri: String, hrefPrefix: String = "", blankNode: String = "",
     editable: Boolean = false,
     actionURI: String = "/save",
-    lang: String = "en"): Try[Elem] = {
+    lang: String = "en", graphURI: String = ""): Try[Elem] = {
+
+    val graphURIActual = if (graphURI == "") uri else graphURI
     val dataset = RDFStoreObject.dataset
     if (blankNode != "true") {
       retrieveURI(makeUri(uri), dataset)
@@ -50,7 +55,7 @@ trait TableViewModule
       for (
         // TODO use allNamedGraphs from RDFStoreObject
         allNamedGraphs <- rdfStore.getGraph(dataset, makeUri("urn:x-arq:UnionGraph"))
-      ) yield graf2form(allNamedGraphs, uri, hrefPrefix, blankNode, editable, actionURI, lang)
+      ) yield graf2form(allNamedGraphs, uri, hrefPrefix, blankNode, editable, actionURI, lang, graphURIActual)
     })
     //    MonadicHelpers.tryToFutureFlat(r)
     r.flatMap { identity }
@@ -77,7 +82,8 @@ trait TableViewModule
     hrefPrefix: String = "", blankNode: String = "",
     editable: Boolean = false,
     actionURI: String = "/save",
-    lang: String = "en"): Elem = {
+    lang: String = "en", graphURI: String): Elem = {
+
     val factory = new FormSyntaxFactory[Rdf](graph, preferedLanguage = lang)
     val form = factory.createForm(
       if (blankNode == "true")
@@ -89,20 +95,20 @@ trait TableViewModule
     )
     println("form:\n" + form)
     val htmlForm = generateHTML(
-      form, hrefPrefix, editable, actionURI)
+      form, hrefPrefix, editable, actionURI, graphURI)
     htmlForm
   }
 
   def htmlFormString(uri: String,
     editable: Boolean = false,
-    actionURI: String = "/save"): String = {
+    actionURI: String = "/save", graphURI: String): String = {
     val f = htmlFormElem(uri, editable = editable, actionURI = actionURI)
     val pp = new PrettyPrinter(80, 2)
     pp.format(f)
   }
 
-  def graf2formString(graph1: Rdf#Graph, uri: String): String = {
-    graf2form(graph1, uri).toString
+  def graf2formString(graph1: Rdf#Graph, uri: String, graphURI: String): String = {
+    graf2form(graph1, uri, graphURI = graphURI).toString
   }
 
   /**

scala/forms_play/dump.sh
@@ -1,0 +1,8 @@
+for f in lib/*.jar
+do
+  JARS=$JARS:$f
+done
+echo java -cp $JARS
+java -cp $JARS tdb.tdbdump --loc=TDB > dump.nt
+ls -l dump.nt
+echo DONE

commit 6cb9e0a3e064eb17f8912aecf75bbd28d5474e33

FIX updating in form: the root cause was that new value was saved in the no-name graph ( bug introduced in https://github.com/jmvanel/semantic_forms/commit/b505b0b002aeacfca0778084c2c80b3dd3117bf1 )

scala/forms/src/main/scala/deductions/runtime/services/FormSaver.scala
@@ -50,20 +50,22 @@ class FormSaver[Rdf <: RDF]()(
       case Some(uri0) =>
         //        val graphURI = URLDecoder.decode(uri0, "utf-8")
         val uri = URLDecoder.decode(uri0, "utf-8")
-        val graphURI = URLDecoder.decode(graphURIOption.getOrElse(uri0), "utf-8")
+        val graphURI =
+          if (graphURIOption == Some("")) uri
+          else URLDecoder.decode(graphURIOption.getOrElse(uri0), "utf-8")
         val v = map.map {
           case (prop0, obj) =>
             val prop = URLDecoder.decode(prop0, "utf-8")
             saveTriplesForProperty(uri, prop, obj, map)
         }
-        doSave(graphURI: String)
+        doSave(graphURI)
       case _ =>
     }
 
     // ==== end of body of saveTriples ====
 
     def saveTriplesForProperty(uri: String, prop: String, objects: Seq[String], map: Map[String, Seq[String]]) = {
-      objects.map(object_ => saveTriple(uri, prop, object_))
+      objects.map(object_ => setTripleChanges(uri, prop, object_))
       println("triplesToRemove " + triplesToRemove)
       println("triples To add " + triples)
     }
@@ -92,7 +94,8 @@ class FormSaver[Rdf <: RDF]()(
       }
     }
 
-    def saveTriple(uri: String, prop: String, obj0: String): Unit = {
+    /** populate lists of triples to add and remove */
+    def setTripleChanges(uri: String, prop: String, obj0: String): Unit = {
       val obj = URLDecoder.decode(obj0, "utf-8")
       println("saveTriple: " + prop + " \"" + obj + "\"")
       if (prop != "url" &&
@@ -101,7 +104,7 @@ class FormSaver[Rdf <: RDF]()(
         !prop.startsWith("ORIG-")) {
         val (changed, originalValue) = processChange(uri, prop, obj)
         if (changed) {
-          println("saveTriple: changed")
+          println(s"""saveTriple: $prop changed from $originalValue to "$obj" """)
           triples +=
             makeTriple(
               makeUri(uri),