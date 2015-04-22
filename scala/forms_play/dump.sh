090c72d7cbdc490bf5ee866b31aa01efbf828064
Arie Benichou <arie.benichou@gmail.com>
2015-04-22 12:45:54 +0200

    moved shell scripts back to the project root

---
+++ scala/forms_play/dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###############################################################################
+# This script is usefull when the application has been packaged with "sbt dist"
+# (see README.md), the resulting zip has been unzipped on the target server.
+###############################################################################
+
+for f in lib/*.jar
+do
+  JARS=$JARS:$f
+done
+echo java -cp $JARS
+java -cp $JARS tdb.tdbdump --loc=TDB > dump.nt
+ls -l dump.nt
+echo DONE
---
+++ scala/forms_play/update_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "update semantic_forms Play! server when code has changed"
+SRC=$HOME/src/semantic_forms/scala/forms_play/
+APP=semantic_forms_play
+APPVERS=${APP}-1.0-SNAPSHOT
+
+cd $SRC
+git pull --verbose
+./activator dist
+echo "sofware recompiled!"
+
+cd ~/deploy
+kill `cat ${APPVERS}/RUNNING_PID`
+
+# pour garder les logs
+rm -r ${APPVERS}_OLD
+mv ${APPVERS} ${APPVERS}_OLD
+
+unzip $SRC/target/universal/${APPVERS}.zip
+
+cd ${APPVERS}
+ln -s ../TDBsf TDB
+nohup bin/${APP} -mem 100 -J-server -Dhttp.port=9000 &
