344890ac0dd6ea0ca151eb5630914d02d66013d9
Arie Benichou <arie.benichou@gmail.com>
2015-04-22 12:18:12 +0200

    moved sh scripts to folder bin and ensured each file is executable

--- scala/forms_play/dump.sh
+++
@@ -1,8 +0,0 @@
-for f in lib/*.jar
-do
-  JARS=$JARS:$f
-done
-echo java -cp $JARS
-java -cp $JARS tdb.tdbdump --loc=TDB > dump.nt
-ls -l dump.nt
-echo DONE
--- scala/forms_play/graphremove.sh
+++
@@ -1,9 +0,0 @@
-GRAPH=$1
-echo "Enlever un graphe $GRAPH dans la base TDB"
-echo "DROP GRAPH <$GRAPH>" > /tmp/delete_graph.rq
-
-sbt <<EOF
-runMain tdb.tdbupdate --loc=TDB --verbose --update=/tmp/delete_graph.rq
-EOF
-
-echo "Local SPARQL database in TDB/ : Enlevé le graphe $GRAPH."
