140bbcbc06c2a1a512badee4bff7082ccc57509f
jmvanel <jeanmarc.vanel@gmail.com>
2015-06-27 23:13:55 +0200

    the button to add a new value now creates an empty content and gives focus.

--- scala/forms_play/public/javascripts/formInteractions.js
+++ scala/forms_play/public/javascripts/formInteractions.js
@@ -1,12 +1,14 @@
 "use strict";
 /*jslint browser: true*/
 
-/** button with an action to duplicate the original HTML widget with (TODO) an empty content */
+/** button with an action to duplicate the original HTML widget with an empty content */
 function cloneWidget(widgetName) {
     var existingWidget =  window.document.getElementsByName(widgetName)[0],
         addWidget = existingWidget.cloneNode(true),
         parent = existingWidget.parentNode;
+    addWidget.value = '';
     parent.insertBefore(addWidget, existingWidget);
+    addWidget.focus();
 }
 
 function backlinks(uri) {
