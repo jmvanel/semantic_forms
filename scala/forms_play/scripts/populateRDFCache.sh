3d9b79dade620e26e603a5ef6684e5e689a6923b
jmvanel <jeanmarc.vanel@gmail.com>
2016-01-30 15:37:40 +0100

    Add scripts to download dbPedia for a specific language, and populate RDF Cache with this.

--- scala/forms_play/scripts/populateRDFCache.sh
+++ scala/forms_play/scripts/populateRDFCache.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sbt "runMain deductions.runtime.sparql_cache.PopulateRDFCache"
+
+echo You may now want to run "download-dbpedia.sh" and "populate_with_dbpedia.sh"
---
+++ scala/forms_play/scripts/populate_with_dbpedia.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Have you downloaded DBPEDIA with script download-dbpedia.sh ?
+echo Beware: This script can take hours! You may have to start from a virgin database and use tdbloader2 instead of tdbloader, see https://jena.apache.org/documentation/tdb/commands.html#tdbloader
+read ZZ
+
+DBPEDIA_VERSION=2015-04
+DBPEDIA_DIR=~/data/dbpedia.org/$DBPEDIA_VERSION
+
+source `dirname $0`/graphload.sh
+
+for file in $DBPEDIA_DIR/*.ttl
+do
+	echo graphload $file http://dbpedia.org/$DBPEDIA_VERSION
+	graphload $file http://dbpedia.org/$DBPEDIA_VERSION
+	echo DONE "$?" graphload $file http://dbpedia.org/$DBPEDIA_VERSION
+done
