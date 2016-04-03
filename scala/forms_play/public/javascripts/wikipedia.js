c7087386040bf1c7a4f2698391cc1da237caba32
jmvanel <jeanmarc.vanel@gmail.com>
2016-04-03 11:37:37 +0200

    dbpedia completion with jquery autocomplete:
    - show URI short definition like it was with datalist implementation

--- scala/forms_play/public/javascripts/wikipedia.js
+++ scala/forms_play/public/javascripts/wikipedia.js
@@ -1,32 +1,24 @@
-// https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest
 // https://github.com/dbpedia/lookup
-// http://stackoverflow.com/questions/148901/is-there-a-better-way-to-do-optional-function-parameters-in-javascript
 // wget "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=10&QueryString=berl" --header='Accept: application/json'
 
-/* TODO pulldown menu in <input> does not show on Chrome and Opera
-see http://blog.teamtreehouse.com/creating-autocomplete-dropdowns-datalist-element 
+/* pulldown menu in <input> does show on Chrome; Opera, Android ????
+see
+https://jqueryui.com/autocomplete/
 https://github.com/RubenVerborgh/dbpedia-lookup-page
+alternate implementation, abandoned: http://blog.teamtreehouse.com/creating-autocomplete-dropdowns-datalist-element 
+TODO: translate in Scala:
 https://github.com/scala-js/scala-js-jquery
 https://www.google.fr/search?q=ajax+example+scala.js
  */
 
-var resultsCount = 20;
+var resultsCount = 15;
 var urlReqPrefix = "http://lookup.dbpedia.org/api/search.asmx/PrefixSearch?QueryClass=&MaxHits=" +
       resultsCount + "&QueryString=" ;
 // var urlReqPrefix = "/lookup?q=";
-var req = new XMLHttpRequest();
 
 /** onkeyup callback on <input> tag */
 function onkeyupComplete( element ) {
-//  function dbpedia_callback(aEvt) {
-//    if (req.readyState == 4) {
-//     console.log( "req.response length " + Object.keys(req.response).length );
-//     var response= eval( '(' + req.responseText + ')' ); // TODO eval not secure!
-//     populate_pulldown_menu( element, response.results );
-//    };
-//  };
   console.log( "onkeyupComplete " + this );
-//  dbpediaComplete( element, dbpedia_callback);
 };
 
 function addDBPediaLookup( inputElementId ) {
@@ -47,19 +39,17 @@ function addDBPediaLookup( inputElementId ) {
               ", ui.value " + ui.item.value );
             console.log( "typeof inputElement " + typeof inputElement );
             console.log( "  inputElement.value ", inputElement.value );
-            // inputElement.value = topics[ui.item.label];
-            // $topics.value = topics[ui.item.label];
        },
         source: function(request, callback) {
           $.ajax({
             url: "http://lookup.dbpedia.org/api/search/PrefixSearch",
-            data: { MaxHits: 15, QueryString: request.term },
+            data: { MaxHits: resultsCount, QueryString: request.term },
             dataType: "json",
             success: function (response) {
               callback(response.results.map(function (m) {
                 console.log( "response " + m );
                 topics[m.label] = m.uri;
-                return { "label": m.label, "value": m.uri }  // m.label;
+                return { "label": m.label + " - " + m.description, "value": m.uri }  // m.label;
               }));
             }
           });
