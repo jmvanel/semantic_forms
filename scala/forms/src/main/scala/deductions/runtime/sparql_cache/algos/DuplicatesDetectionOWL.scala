8a2403f9566e46b8027957104bbf10e00a3a0938
jmvanel <jeanmarc.vanel@gmail.com>
2016-06-03 12:01:21 +0200

    DuplicatesDetectionOWL.scala (WIP) : outputs identical pairs :(

--- scala/forms/src/main/scala/deductions/runtime/sparql_cache/algos/DuplicatesDetectionOWL.scala
+++ scala/forms/src/main/scala/deductions/runtime/sparql_cache/algos/DuplicatesDetectionOWL.scala
@@ -1,6 +1,7 @@
 package deductions.runtime.sparql_cache.algos
 
 import java.util.StringTokenizer
+import java.io.FileReader
 
 import org.w3.banana.OWLPrefix
 import org.w3.banana.PointedGraph
@@ -8,6 +9,20 @@ import org.w3.banana.RDF
 import org.w3.banana.RDFOps
 import org.w3.banana.RDFPrefix
 import org.w3.banana.RDFSPrefix
+import org.w3.banana.jena.JenaModule
+import org.w3.banana.jena.Jena
+
+// deductions.runtime.sparql_cache.algos.DuplicatesDetectionOWLApp
+object DuplicatesDetectionOWLApp extends App with JenaModule with DuplicatesDetectionOWL[Jena] {
+  val owlFile = args(0)
+  val graph = turtleReader.read( new FileReader(owlFile), "") .get
+  val duplicates = findDuplicateDataProperties(graph)
+  println( s"duplicates size ${duplicates.duplicates.size}")
+  val v = duplicates.duplicates.map { dup => dup toString(graph) }
+  println( v . mkString("\n") )
+  println( s"duplicates size ${duplicates.duplicates.size}")
+}
+
 
 trait DuplicatesDetectionOWL[Rdf <: RDF] {
 
@@ -17,35 +32,63 @@ trait DuplicatesDetectionOWL[Rdf <: RDF] {
   private val rdfs = RDFSPrefix[Rdf]
   private val owl = OWLPrefix[Rdf]
 
-  def findDuplicateDataProperties(graph: Rdf#Graph) = {
-    val datatypeProperties = find(graph, ANY, rdf.typ, owl.DatatypeProperty)
+  case class Duplicate(d1: Rdf#Node, d2: Rdf#Node) {
+    def toString(graph: Rdf#Graph): String = {
+      def toString(n: Rdf#Node) =
+        d1.toString().replace("http://data.onisep.fr/ontologies/", ":") +
+          " \"" + rdfsLabel(d1, graph) + "\"";
 
-    val datatypePropertiesURI = datatypeProperties.map{ trip => trip.subject }
-    val datatypePropertiesPairs = datatypePropertiesURI.toSet.subsets(2)
-    // TODO réécrire boucle ci-dessous avec datatypePropertiesPairs
-    
-    for {
-      datatypeProperty_triple1 <- datatypeProperties
-      datatypeProperty_triple2 <- datatypeProperties
-      datatypeProperty1 = datatypeProperty_triple1.subject
-      datatypeProperty2 = datatypeProperty_triple2.subject
-      if (datatypeProperty1 != datatypeProperty2)
-    } {
-      val ranges1 = find(graph, datatypeProperty1, rdfs.range, ANY )
-      val ranges2: Iterator[Rdf#Triple] = find(graph, datatypeProperty2, rdfs.range, ANY )
-      val rangesOverlap = ! ranges1.toSet.intersect(ranges2.toSet) . isEmpty
-      
-      val label1 = ( PointedGraph( datatypeProperty1, graph ) / rdfs.label ) . as[String] . getOrElse("")
-      val label2 = ( PointedGraph( datatypeProperty2, graph ) / rdfs.label ) . as[String] . getOrElse("")
-      
-      areSimilar( label1, label2 )
-      // TODO ???????????? @return the list of pairs of similar property URI's
+      toString(d1) +
+      " ~ " +
+      toString(d2)
+    }
+  }
+  case class DuplicationAnalysis( duplicates: List[Duplicate] )
+
+  
+  /** @return the list of pairs of similar property URI's */
+  def findDuplicateDataProperties(graph: Rdf#Graph): DuplicationAnalysis = {
+	  println(s"Triple count ${graph.size}")
+    val datatypeProperties = find(graph, ANY, rdf.typ, owl.DatatypeProperty)
+    val datatypePropertiesURI = datatypeProperties.map { triple => triple.subject } . toList
+    println(s"datatype Properties count ${datatypePropertiesURI.size}")
+    val datatypePropertiesPairs = datatypePropertiesURI.toSet.subsets(2). toList
+    println(s"datatype Properties pairs count ${datatypePropertiesPairs.size}")
+    val pairs = for {
+      pair <- datatypePropertiesPairs if (haveSimilarLabels(pair, graph))
+    } yield {
+      pair.toList match {
+        case (datatypeProperty1 :: datatypeProperty2 :: rest) => Duplicate(datatypeProperty1, datatypeProperty2)
+        case _ => Duplicate(???, ???) // will not happen :)
+      }
+    }
+    DuplicationAnalysis( pairs.toList )
+  }
+
+  def haveSimilarLabels(pair: Set[Rdf#Node], graph: Rdf#Graph): Boolean = {
+    pair.toList match {
+      case (datatypeProperty1 :: datatypeProperty2 :: rest) =>
+        val ranges1 = find(graph, datatypeProperty1, rdfs.range, ANY)
+        val ranges2: Iterator[Rdf#Triple] = find(graph, datatypeProperty2, rdfs.range, ANY)
+        val rangesOverlap = !ranges1.toSet.intersect(ranges2.toSet).isEmpty
+
+        val label1 = rdfsLabel( datatypeProperty1, graph)
+        val label2 = rdfsLabel( datatypeProperty2, graph)
+        areSimilar(label1, label2)
+      case _ => false
     }
   }
 
   def areSimilar(s1: String, s2: String): Boolean = {
-    val st1 = new StringTokenizer(s1)
-    val st2 = new StringTokenizer(s2)
-    ???
+    val words1 = s1.split("""\s+""").toSet
+    val words2 = s2.split("""\s+""").toSet
+    val intersection = words1 intersect (words2)
+    val averageSize = ( words1.size +  words2.size ) * 0.5
+    if( intersection.size > 0 )
+      println(s"""intersection.size >0  $intersection - "$s1" "$s2" """)
+    intersection.size > averageSize * 0.5
   }
+  
+  def rdfsLabel( n: Rdf#Node, graph: Rdf#Graph) =
+		  (PointedGraph( n , graph) / rdfs.label).as[String].getOrElse("")
 }
