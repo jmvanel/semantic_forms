0cb85a35beacfe1773b2fd1865785243845accff
jmvanel <jeanmarc.vanel@gmail.com>
2016-07-09 11:05:39 +0200

    TextTreeViewRDF: also print inheritance Roots ?R that are not declared by ?R a owl:Class ;
    sort lines by rdfs:label

--- scala/forms/src/main/scala/deductions/runtime/sparql_cache/algos/TextTreeViewRDF.scala
+++ scala/forms/src/main/scala/deductions/runtime/sparql_cache/algos/TextTreeViewRDF.scala
@@ -33,10 +33,17 @@ object TextTreeViewRDF extends App with JenaModule with DuplicatesDetectionBase[
   output(s"$report")
   outputErr(s"File written: $outputFile")
 
+  import ops._
+
   /** format tree view with tabs */
   def formatTreeView(instancesURI: List[Rdf#Node]) = {
     val roots = findRoots(instancesURI)
-    roots foreach { indentURILabel( _, 0) }
+    val roots2 = findUndeclaredRoots(instancesURI)
+    val undeclaredRoots = roots2.toSet.diff(roots.toSet)
+    println(s"Undeclared Roots: ${undeclaredRoots.size} : $undeclaredRoots")
+    val sortedMergedRoots = sortWithRdfsLabel((roots ::: roots2).toSet.toList)
+    println("sorted Merged Roots: " + sortedMergedRoots.size)
+    sortedMergedRoots.foreach { indentURILabel(_, 0) }
   }
 
   /** TODO parameterize % predicate to build tree: currently hard-coded rdfs:subClassOf */
@@ -47,13 +54,22 @@ object TextTreeViewRDF extends App with JenaModule with DuplicatesDetectionBase[
     ) yield instance
   }
 
+  /** find inheritance Roots ?R that are not declared by 
+   *  ?R a owl:Class */
+  def findUndeclaredRoots(instancesURI: List[Rdf#Node]): List[Rdf#Node] = {
+	  val tr = find(graph, ANY, rdfs.subClassOf, ANY ) . map { tr => tr.objectt } . toList
+    tr . toSet . toList
+  }
+
   /** recursive */
   def indentURILabel(node: Rdf#Node, depth: Int): Unit = {
     val formattedURILabel = ("\t" * depth) + rdfsLabel(node, graph) +" - " + abbreviateURI( node )
     output( formattedURILabel )
     // recursive call 
     val subClasses = rdfsSubClasses( node, graph)
-    subClasses.foreach { indentURILabel( _, depth+1) }
+    sortWithRdfsLabel(subClasses).foreach { indentURILabel( _, depth+1) }
   }
 
+  def sortWithRdfsLabel(list: List[Rdf#Node]) = list.sortWith(
+      (n1, n2) => rdfsLabel(n1, graph) < rdfsLabel(n2, graph))
 }
