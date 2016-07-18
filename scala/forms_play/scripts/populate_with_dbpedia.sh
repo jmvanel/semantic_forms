17d1c7c2b8b21fb2667a371861fcc7d2f9dc7563
jmvanel <jeanmarc.vanel@gmail.com>
2016-07-18 13:13:24 +0200

    tested scripts/populate_with_dbpedia.sh
    removed load-dbpedia.sh

--- scala/forms_play/scripts/populate_with_dbpedia.sh
+++ scala/forms_play/scripts/populate_with_dbpedia.sh
@@ -6,15 +6,18 @@ echo Press RETURN
 read WAIT
 
 DBPEDIA_VERSION=2015-10
-DBPEDIA_DIR=~/data/dbpedia.org/$DBPEDIA_VERSION
-DBPEDIA_GRAPH_URI=~/data/dbpedia.org/$DBPEDIA_VERSION
+DBPEDIA_DIR=$HOME/data/dbpedia.org/$DBPEDIA_VERSION
+DBPEDIA_GRAPH_URI=file://$HOME/data/dbpedia.org/$DBPEDIA_VERSION
+SFDIR=$HOME/src/semantic_forms/scala/forms_play
 
 for file in $DBPEDIA_DIR/*.ttl
 do
 	echo File $file $DBPEDIA_GRAPH_URI
-	sed -e"s/.$/<dbpedia:$file>./" $file > $file.nt
-	# graphload $file $DBPEDIA_GRAPH_URI
-	echo DONE, return code "$?" : created NT file $file.nt with single named graph $DBPEDIA_GRAPH_URI
+	sed -e"s=.$=<dbpedia:$file>.=;/^#/d" $file > $file.nq
+	echo DONE, return code "$?" : created NT file $file.nq with single named graph $DBPEDIA_GRAPH_URI
+	ls -l $file.nq
 done
 
-$HOME/src/jena/apache-jena/bin/tdbloader2 --loc ../TDB $DBPEDIA_DIR/*.ttl.nt
+$HOME/apps/apache-jena-3.0.1/bin/tdbloader2 --loc $SFDIR/TDB $DBPEDIA_DIR/*.ttl.nq
+# Download Jena from http://archive.apache.org/dist/jena/binaries/apache-jena-2.13.0.tar.gz
+# $HOME/apps/apache-jena-2.13.0/bin/tdbloader2 --loc $SFDIR/TDB $DBPEDIA_DIR/*.ttl.nq
