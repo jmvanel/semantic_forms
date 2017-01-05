c31056ceff063fd958b91e8b5e949641f2ec5ca7
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-01-05 19:59:20 +0100

    showing form subject URI in form header, FIX #119 "Generate more HTML thumbnails for URI's"

--- scala/forms/src/main/scala/deductions/runtime/views/FormHeader.scala
+++ scala/forms/src/main/scala/deductions/runtime/views/FormHeader.scala
@@ -23,7 +23,7 @@ trait FormHeader[Rdf <: RDF]
   val config: Configuration
   import config._
 
-  /** TODO <<<<<<<<<<<<<< */
+  /** title and links on top of the form: Edit, Display, Download Links */
   def titleEditDisplayDownloadLinksThumbnail(formSyntax: FormSyntax, lang: String, editable: Boolean = false)(implicit graph: Rdf#Graph): NodeSeq = {
     def mess(m: String) = I18NMessages.get(m, lang)
     val uri = nodeToString(formSyntax.subject)
@@ -52,6 +52,13 @@ trait FormHeader[Rdf <: RDF]
             { linkToShow }
             { expertLinks }
           </strong>
+          {
+            if (formSyntax.thumbnail.isDefined)
+              <img src={ formSyntax.thumbnail.get.toString() } css="sf-thumbnail" height="40" alt={
+                s"Image of ${formSyntax.title}: ${formSyntax.subject.toString()}"
+              }/>
+              else NodeSeq.Empty
+          }
         </h3>
       </div>
     </div>
@@ -74,53 +81,4 @@ trait FormHeader[Rdf <: RDF]
     </div>
   }
 
-  /** title and links on top of the form: Edit, Display, Download Links */
-  def titleEditDisplayDownloadLinks(uri: String, lang: String, editable: Boolean = false)(implicit graph: Rdf#Graph): NodeSeq = {
-    def mess(m: String) = I18NMessages.get(m, lang)
-
-    val linkToShow = (if (editable) {
-      <a class="btn btn-warning" href={ hrefDisplayPrefix + URLEncoder.encode(uri, "utf-8") } title={ mess("display_URI") }>
-        <i class="glyphicon glyphicon-remove"></i>
-      </a>
-    } else {
-      <a class="btn btn-primary" href={ hrefEditPrefix + URLEncoder.encode(uri, "utf-8") } title={ mess("edit_URI") }>
-        <i class="glyphicon glyphicon-edit"></i>
-      </a>
-    })
-
-    val expertLinks = (if (showExpertButtons) {
-      Seq(makeBackLinkButton(uri),
-        new Text("  "),
-        makeDrawGraphLink(uri))
-    } else new Text(""))
-
-    <div class="container">
-      <div class="row">
-        <h3>
-          { labelForURI(uri, lang) }
-          <strong>
-            { linkToShow }
-            { expertLinks }
-          </strong>
-        </h3>
-      </div>
-    </div>
-    <div class="sf-links-row">
-      <!--div class="col-md-6"-->
-      <div class="sf-local-rdf-link">
-        {
-          val message = if (uri.contains("/ldp/"))
-            "Download local URI"
-          else
-            mess("Download_original_URI")
-          <a href={ uri }>{ message }</a>
-        }
-      </div>
-      <div class="sf-local-rdf-link">
-        <a href={ hrefDownloadPrefix + URLEncoder.encode(uri, "utf-8") } title={ mess("Triples_tooltip") }>
-          { mess("Triples") }
-        </a>
-      </div>
-    </div>
-  }
 }
