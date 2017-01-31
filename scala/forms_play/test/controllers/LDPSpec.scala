1771c360fb01c9695c4ca38a8bd8dbd4a9e0ef4a
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-01-31 20:18:24 +0100

    FIX compile errors in Play! test LDPSpec.scala after Play 2.5.2;
    but error:
    [info]   java.lang.NoSuchMethodError: play.api.test.FakeApplication$.$lessinit$greater$default$3()Lscala/collection/Seq;
    [info]   at org.scalatestplus.play.OneAppPerSuite$class.app(OneAppPerSuite.scala:126)
    [info]   at controllers.TestAuth.app$lzycompute(TestAuth.scala:15)
    [info]   at controllers.TestAuth.app(TestAuth.scala:15)

--- scala/forms_play/test/controllers/LDPSpec.scala
+++ scala/forms_play/test/controllers/LDPSpec.scala
@@ -15,7 +15,14 @@ import play.api.test.Helpers.contentAsString
 import play.api.test.Helpers.contentType
 import play.api.test.Helpers.defaultAwaitTimeout
 import play.api.test.Helpers.status
+import scala.concurrent.Future
+import play.api.mvc.Result
 
+import play.api.mvc._
+import play.api.test._
+import play.api.test.Helpers._
+import org.scalatestplus.play.OneAppPerSuite
+import akka.stream.Materializer
 
 /** 
  *  https://www.playframework.com/documentation/2.4.x/ScalaTestingWithScalaTest
@@ -48,7 +55,8 @@ The triples for this test are stored in this named graph:
  * cf http://www.w3.org/TR/ldp-primer/#creating-an-rdf-resource-post-an-rdf-resource-to-an-ldp-bc */
 class LDPSpec extends PlaySpec
     with WhiteBoxTestdependencies
-    with OneAppPerTest {
+    with  OneAppPerSuite {
+
   val config = new DefaultConfiguration {
     override val useTextQuery = false
     override val needLoginForEditing = false
@@ -66,23 +74,25 @@ class LDPSpec extends PlaySpec
 
   val timeout: Timeout = Timeout( DurationInt(40) seconds )
 
-  "LDP service" must {
+  "LDP service" should {
     "respond to the ldp POST and GET Actions" in {
       post()
       get()
     }
   }
-  
+
+  implicit lazy val materializer: Materializer = app.materializer
+
+  /** cf https://www.playframework.com/documentation/2.5.x/ScalaTestingWithScalaTest#Unit-Testing-EssentialAction */
   def post() {
     val request = FakeRequest( Helpers.POST, appURL ).
       withHeaders(
           ("Slug", file),
           ("Content-Type", "text/turtle")
       ). withTextBody(bodyTTL)
-    val result0 = Application.ldpPOSTAction(ldpContainerURI)(request)
-    val enum: Enumerator[Array[Byte]] = Enumerator( bodyTTL.getBytes )
-    val result = enum run result0 
-
+    val action = Application.ldpPOSTAction(ldpContainerURI)
+    val result = call(action, request)
+      
     info( s"POST to URL $appURL")
     info(  "POST status: " + status(result)(timeout) ) // must equalTo(OK)
     info( s"POST ${contentType(result)(timeout)}" ) // must beSome("text/plain")
@@ -93,29 +103,32 @@ class LDPSpec extends PlaySpec
     val graph = getGraph(ldpDataFileURI)
     info( s"""POST: getGraph($ldpDataFileURI):
         "$graph" """ )
-      
+
     // NOTE: this assertion does not work, because TDB in object controllers.Application was updated,
     // and we query another TDB instance (in the same directory tough but that's not enough!)
     // assert( graph.contains("Salut!") )
-
-    val query = s"SELECT * WHERE { GRAPH <$ldpDataFileURI> {?S ?P ?O.}}"
-    info( s"query $query" )
-    val r = Application.select( query ) (FakeRequest( Helpers.GET, "" ) )
-    val result1 = enum run r
-    val sresult: String = contentAsString( result1 )
-    info( "Application.sparql: " + sresult )
-    sresult.substring( sresult.length() - 200 )
-//    assert( sresult.contains("Salut!") ) // For reason unknown this fails too !!!!!!!!!!
-    //    info( s"""POST:assert succeded!""" )
+    {
+      val query = s"SELECT * WHERE { GRAPH <$ldpDataFileURI> {?S ?P ?O.}}"
+      info(s"query $query")
+      val action = Application.select(query)
+      val request = FakeRequest(Helpers.GET, "")
+      val result = call(action, request)
+      val sresult: String = contentAsString(result)
+      info("Application.sparql: " + sresult)
+      sresult.substring(sresult.length() - 200)
+      //    assert( sresult.contains("Salut!") ) // For reason unknown this fails too !!!!!!!!!!
+      //    info( s"""POST:assert succeded!""" )
+    }
   }
   
   def get() {
     val getRelativeURI = appURL + file
     info( s"""GET: $getRelativeURI""" )
 	  val request = FakeRequest( Helpers.GET, getRelativeURI ).
-    withHeaders(( "Accept", "text/turtle")) // , application/ld+json") )
-    val result0 = Application.ldp(ldpContainerURI + file).apply(request)
-    val enum: Enumerator[Array[Byte]] = Enumerator() ; val result = enum run result0 
+	  withHeaders(( "Accept", "text/turtle")) // , application/ld+json") )
+    val action = Application.ldp(ldpContainerURI + file)
+    val result = call(action, request)
+//    val enum: Enumerator[Array[Byte]] = Enumerator() ; val result = enum run result0 
     val content = contentAsString(result)(timeout)
     info( s"""GET: contentAsString: "$content" """ )
     assert( content.contains("Salut!") )
