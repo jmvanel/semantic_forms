6fd734fa65c278fa8ad8e70159881983a470cc59
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-02-13 12:06:02 +0100

    FIX ISSUE #125 : added the verification if the HTTP Parameters "tabs" is set or not.

--- scala/forms/src/main/scala/deductions/runtime/html/Form2HTML.scala
+++ scala/forms/src/main/scala/deductions/runtime/html/Form2HTML.scala
@@ -174,7 +174,7 @@ private[html] trait Form2HTML[NODE, URI <: NODE]
                 }
               </div> ++
               {
-                if (groupFields) {
+                if (request.headers.contains("tabs")) {
                   makeFieldsGroups()
                 } else
                   makeFieldsLabelAndData(form.fields)
