5b7d9bec77d4f3240851aa73669760e1b269c7c3
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-03-05 11:47:18 +0100

    log user ID

--- scala/forms_play/app/controllers/RequestUtils.scala
+++ scala/forms_play/app/controllers/RequestUtils.scala
@@ -1,15 +1,17 @@
 package controllers
 
-import java.text.DateFormat
-import java.util.Date
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import play.api.mvc.Request
 
 trait RequestUtils {
-  def log(mess: String, request: Request[_]) =
-    DateFormat.getInstance.format(new Date()) +
-      s" $mess: IP ${request.remoteAddress} $request, id ${request.id}, host ${request.host}"
 
-  def userId(request: Request[_]) = {
+  def log(mess: String, request: Request[_]) =
+		  LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) +
+      s""" $mess: IP ${request.remoteAddress}, userId "${userId(request)}", $request, id ${request.id}, host ${request.host}"""
+
+  def userId(request: Request[_]): String = {
     val usernameFromSession = for (
       cookie <- request.cookies.get("PLAY_SESSION");
       value = cookie.value
