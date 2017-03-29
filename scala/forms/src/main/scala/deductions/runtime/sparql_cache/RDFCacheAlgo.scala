2872cda833aaa39a7a06e854429863e25c525b7d
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-03-29 08:33:02 +0200

    FIX FEATURE: annotate plain Web site: a transaction was missing , in pureHTMLwebPageAnnotateAsDocument()

--- scala/forms/src/main/scala/deductions/runtime/sparql_cache/RDFCacheAlgo.scala
+++ scala/forms/src/main/scala/deductions/runtime/sparql_cache/RDFCacheAlgo.scala
@@ -408,10 +408,10 @@ trait RDFCacheAlgo[Rdf <: RDF, DATASET] extends RDFStoreLocalProvider[Rdf, DATAS
       URI(makeAbsoluteURIForSaving(request.userId())),
       newGraphWithUrl)
     println(s"getLocallyManagedUrlAndData: saved $newGraphWithUrl in graph <${makeAbsoluteURIForSaving(request.userId())}>")
-
+    val it = wrapInReadTransaction { find(allNamedGraph, uri, ANY, ANY)  } . getOrElse( Iterator.empty )
     newGraphWithUrl .
     // NOTE: after user added triples, this way typeChange will not be triggered
-    union( makeGraph(find(allNamedGraph, uri, ANY, ANY).toIterable))
+    union( makeGraph(it.toIterable))
   }
 }
 
