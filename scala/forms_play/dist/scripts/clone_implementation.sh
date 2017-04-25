a6d7752448ce7c0e732f032100dc08fbd467edc1
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-04-25 17:12:54 +0200

    (probably) FIX dist/scripts/clone_implementation.sh

--- scala/forms_play/dist/scripts/clone_implementation.sh
+++ scala/forms_play/dist/scripts/clone_implementation.sh
@@ -2,9 +2,10 @@
 echo clone SF implementation, i.e. clone an unzipped SF distribution
 #  use case: reuse the implementation with different databases or settings
 
-mkdir ../semantic_forms_cloned
+CLONE=../semantic_forms_cloned
+mkdir $CLONE
 
-for f in bin en fr lib README.md scripts ; do ln -s $PWD/$f ../semantic_forms_cloned/$f ; echo ln -s $PWD/$f ../semantic_forms_cloned/$f ; done
+for f in en fr lib README.md  ; do ln -s $PWD/$f $CLONE/$f ; echo ln -s $PWD/$f ../semantic_forms_cloned/$f ; done
 
-for f in conf files ; do cp -r $f ../semantic_forms_cloned/ ; echo cp -r $f ../semantic_forms_cloned/ ; done
-echo current SF implementation cloned in ../semantic_forms_cloned
+for f in conf files scripts bin ; do cp -r $f $CLONE ; echo cp -r $f $CLONE ; done
+echo current SF implementation cloned in $CLONE , you might want to rename it.
