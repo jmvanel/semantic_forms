3f5d2f93e50b756ef8eabb31074543311bca50ec
TristanMatteï <mattei.tristan@gmail.com>
2017-05-24 11:15:46 +0200

    ADD cornichon test for /display

--- scala/web_tests/src/test/scala/deductions/runtime/non_regression/Display.scala
+++ scala/web_tests/src/test/scala/deductions/runtime/non_regression/Display.scala
@@ -15,15 +15,6 @@ class Display extends CornichonFeature {
           "displayuri" -> URLEncoder.encode(url)
         )
           Then assert status.is(200)
-          When i get("http://localhost:9000/download").withParams(
-            "url" -> URLEncoder.encode(url)
-          )
-        Then assert status.is(200)
-          When i get("http://localhost:9000/form-data").withParams(
-            "displayuri" -> URLEncoder.encode(url)
-          )
-          And assert body.path("subject").isPresent
-          Then assert status.is(200)
         }
       }
     }
