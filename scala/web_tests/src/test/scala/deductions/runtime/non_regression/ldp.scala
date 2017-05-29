c012ebf81b82451402c9eff29cdf6c74b366128d
TristanMatteï <mattei.tristan@gmail.com>
2017-05-29 12:01:31 +0200

    ADD cornichon test for /ldp

---
+++ scala/web_tests/src/test/scala/deductions/runtime/non_regression/ldp.scala
@@ -0,0 +1,18 @@
+import java.net.URLEncoder
+import com.github.agourlay.cornichon.CornichonFeature
+
+class LDP extends CornichonFeature {
+  lazy val uri = "1496050844554-8066747071053"
+
+  def feature = Feature("LDP") {
+
+    Scenario("LDP") {
+      RetryMax(5) {
+        When I get("http://localhost:9000/ldp/" + uri)
+        Then assert status.is(200)
+        And assert headers.name("Access-Control-Allow-Origin").isPresent
+
+      }
+    }
+  }
+}
