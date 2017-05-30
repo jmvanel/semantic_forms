09a60a134de17b93b89d6416e8e4fb8e8c3373e5
TristanMatteï <mattei.tristan@gmail.com>
2017-05-30 12:19:53 +0200

    Improve cornichon test for /form-data

--- scala/web_tests/src/test/scala/deductions/runtime/non_regression/form_data.scala
+++ scala/web_tests/src/test/scala/deductions/runtime/non_regression/form_data.scala
@@ -3,23 +3,20 @@ import java.net.URLEncoder
 import com.github.agourlay.cornichon.CornichonFeature
 
 class FORM_DATA extends CornichonFeature {
-  lazy val url =
-    """
-      http://jmvanel.free.fr/jmv.rdf#me
-        """
+  lazy val url ="http://jmvanel.free.fr/jmv.rdf#me"
 
   def feature = Feature("FORM_DATA") {
 
     Scenario("FORM_DATA") {
 
-      get("http://localhost:9000/form-data").withParams(
-        "displayuri" -> URLEncoder.encode(url)
-      )
+      When I get("http://localhost:9000/form-data?displayuri=" + URLEncoder.encode(url))
+
       Then assert status.is(200)
       And assert body.path("subject").isPresent
-      //And assert body.path("form-label").is("Personne FOAF - court")
+      And assert body.path("formLabel").is("FOAF Person - short form")
+      And assert body.path("fields").asArray.isNotEmpty
       And assert body.path("fields[0].subject").isPresent
-      //And assert body.path("fields[0].value").is("Jean-Marc")
+      And assert body.path("fields[0].value").is("Jean-Marc")
 
     }
   }
