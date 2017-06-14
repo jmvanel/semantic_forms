68655e66af6802889fb1ea2bb952fa128ae9bb83
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-06-14 11:20:55 +0200

    compute path length from geo data: TESTED with
    runMain deductions.runtime.sparql_cache.algos.GeoPathApp imei:863977030715952  http://semantic-forms.cccccc/sparql?query=PREFIX+geoloc%3A+%3Chttp%3A%2F%2Fdeductions.github.io%2Fgeoloc.owl.ttl%23%3E%0D%0APREFIX+dct%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0A%0D%0ACONSTRUCT+%7B%0D%0A++%3FDEPART+%3FP+%3FO+.%0D%0A%7D+WHERE+%7B%0D%0A++GRAPH+%3FG+%7B%0D%0A++++++%3FDEPART+geoloc%3Amobile+%3Cimei%3A863977030715952%3E+%3B%0D%0A++++++++++++++%3FP+%3FO+%3B%0D%0A+++++dct%3Adate+%3FDATE+.%0D%0A++%7D++%7D%0D%0AORDER+BY+DESC%28%3FDATE%29%0D%0ALIMIT+20

--- scala/forms/src/main/scala/deductions/runtime/sparql_cache/algos/GeoPath.scala
+++ scala/forms/src/main/scala/deductions/runtime/sparql_cache/algos/GeoPath.scala
@@ -1,42 +1,91 @@
 package deductions.runtime.sparql_cache.algos
 
+import java.net.URL
+import scala.util.Try
+import scala.util.Success
+
 import org.w3.banana.PointedGraph
 import org.w3.banana.RDF
 import org.w3.banana.RDFOps
 import org.w3.banana.PointedGraph
 import org.w3.banana.PointedGraphs
+import org.w3.banana.io._
 
 import deductions.runtime.utils.RDFPrefixes
 import deductions.runtime.utils.RDFHelpers
 import deductions.runtime.utils.RDFPrefixes
 import deductions.runtime.utils.RDFHelpers
+import deductions.runtime.jena.ImplementationSettings
+import deductions.runtime.services.DefaultConfiguration
+
+object GeoPathApp extends {
+  override val config = new DefaultConfiguration {
+    override val useTextQuery = false
+  }
+} with App
+    with ImplementationSettings.RDFCache
+    with GeoPath[ImplementationSettings.Rdf] {
+  import ops._
+
+  val graph = // URI("data:/geo/")
+    rdfLoader.load(new URL(args(1)))
+    println(s"graph $graph")
+    graph.map {
+      graph =>
+        println("graph.map")
+        val pathLength = getPathLengthForMobile(URI(args(0)), graph)
+        println(s"Path Length For Mobile <${URI(args(0))}> ${pathLength} km")
+    }
+}
 
 trait GeoPath[Rdf <: RDF]
     extends RDFPrefixes[Rdf]
-//    with SPARQLHelpers[Rdf, DATASET]
+    //    with SPARQLHelpers[Rdf, DATASET]
     with RDFHelpers[Rdf] {
 
   implicit val ops: RDFOps[Rdf]
   import ops._
+  val rdfLoader: RDFLoader[Rdf, Try]
 
   val precedingPointProp = geoloc("precedingPoint")
   val mobileProp = geoloc("mobile")
 
-  def getPathLengthForMobile(mobile: Rdf#Node, graph: Rdf#Graph) : Float = {
-    getPathLength( getPathForMobile(mobile, graph) )
+  def getPathLengthForMobile(mobile: Rdf#Node, graph: Rdf#Graph): Float = {
+    getPathLength(getPathForMobile(mobile, graph))
   }
 
-  def getPathLength(graph: Iterable[PointedGraph[Rdf]] ): Float = {
-		def pow2(v: Float)= v*v
+  /** get Path Length in kilometers */
+  def getPathLength(graph: Iterable[PointedGraph[Rdf]]): Float = {
+    def pow2(v: Float) = v * v
+    println(s"in getPathLength size ${graph.size}")
     val coords = for (
-      triplesAboutPoint <- graph ;
-      point = triplesAboutPoint.pointer ;
-      graph = triplesAboutPoint.graph ;
-      long = (triplesAboutPoint / geo("long")) .as[Rdf#Literal] .toString .toFloat ;
-      lat = (triplesAboutPoint / geo("lat")) .as[Rdf#Literal] .toString .toFloat
-    ) yield {(long, lat)}
-    val res = coords.reduceLeft((a,b) => (Math.sqrt(pow2(b._1 - a._1) + pow2(b._2 - a._2)).toFloat, 0) )
-    val distanceInDegrees = res._1
+      triplesAboutPoint <- graph;
+      point = triplesAboutPoint.pointer;
+      graph = triplesAboutPoint.graph;
+//      _ = println(s"graph size ${graph.size}");
+      long = (triplesAboutPoint / geo("long")).as[Rdf#Literal]  match {
+        case Success(lit1) => fromLiteral(lit1)._1 . toFloat
+        case _ => Float.NaN // 0f
+      };     
+      lat = (triplesAboutPoint / geo("lat")).as[Rdf#Literal]  match {
+        case Success(lit1) => fromLiteral(lit1)._1 . toFloat
+        case _ => Float.NaN // 0f
+      }
+    ) yield {
+//    	println(s"in getPathLength (long, lat) ${(long, lat)}")
+      (long, lat) }
+    
+    var distanceInDegrees = 0f
+    val res = coords.reduceLeft(
+        (a, b) => {
+          val distanceDelta= Math.sqrt(pow2(b._1 - a._1) + pow2(b._2 - a._2)).toFloat
+//          println( s"distanceInDegrees delta=${distanceDelta} a=$a b=$b")
+          if( ! distanceDelta.isNaN())
+            distanceInDegrees += distanceDelta
+          b
+        }
+    		)
+    println( s"distanceInDegrees $distanceInDegrees")
     distanceInDegrees * 6371 * Math.PI / 180 toFloat
   }
 
@@ -46,10 +95,13 @@ trait GeoPath[Rdf <: RDF]
    */
   def getPathForMobile(mobile: Rdf#Node, graph: Rdf#Graph): Iterable[PointedGraph[Rdf]] = {
     val points = getPointsForMobile(mobile, graph)
+    println(s"called getPointsForMobile size ${points.size}")
     for (
       point <- points;
       triplesAboutPoint = find(graph, point, ANY, ANY)
-    ) yield { PointedGraph( point, makeGraph(triplesAboutPoint.toIterable) )}
+    ) yield {
+//          println(s"getPathForMobile: $point")
+          PointedGraph(point, makeGraph(triplesAboutPoint.toIterable)) }
   }
 
   /** get Points (URI's) For Mobile, ordered by using dct:date */
