649c798d6002e65657f930999978f90d9789f489
TristanMatteï <mattei.tristan@gmail.com>
2017-06-27 13:27:16 +0200

    REFACTOR move "val" value to "def" function in Form2HTMLDisplay

--- scala/forms/src/main/scala/deductions/runtime/html/Form2HTMLDisplay.scala
+++ scala/forms/src/main/scala/deductions/runtime/html/Form2HTMLDisplay.scala
@@ -2,23 +2,25 @@ package deductions.runtime.html
 
 import java.net.URLEncoder
 
+import deductions.runtime.abstract_syntax.FormModule
 import org.joda.time.DateTime
 
 import scala.xml.NodeSeq
 import scala.xml.Text
 import scala.xml.Unparsed
 //import deductions.runtime.views.ToolsPage
-	import deductions.runtime.utils.HTTPrequest
-	import scala.xml.UnprefixedAttribute
+import deductions.runtime.utils.HTTPrequest
+import scala.xml.UnprefixedAttribute
 
 /** generate HTML from abstract Form for Display (Read only) */
 trait Form2HTMLDisplay[NODE, URI <: NODE]
-    extends Form2HTMLBase[NODE, URI] {
+  extends Form2HTMLBase[NODE, URI] {
 
-	import config._
-	import prefixes._
+  import config._
+  import prefixes._
 
-	private[html] def createHTMLiteralReadonlyField(l: formMod#LiteralEntry): NodeSeq =
+
+  private[html] def createHTMLiteralReadonlyField(l: formMod#LiteralEntry): NodeSeq =
     <xml:group>
       <div class={css.cssClasses.formDivInputCSSClass}>{ Unparsed(toPlainString(l.value)) }{makeUserInfoOnTriples(l.metadata,l.timeMetadata)}</div>
 
@@ -26,84 +28,92 @@ trait Form2HTMLDisplay[NODE, URI <: NODE]
     </xml:group>
 
   /** create HTML Resource Readonly Field
-   * PENDING if inner val should need to be overridden, they should be directly in trait
-   *  */
+    * PENDING if inner val should need to be overridden, they should be directly in trait
+    *  */
   def createHTMLResourceReadonlyField(
-      resourceEntry: formMod#ResourceEntry,
-      hrefPrefix: String = hrefDisplayPrefix,
-      request: HTTPrequest = HTTPrequest()
-      ): NodeSeq = {
+                                       resourceEntry: formMod#ResourceEntry,
+                                       hrefPrefix: String = hrefDisplayPrefix,
+                                       request: HTTPrequest = HTTPrequest()
+                                     ): NodeSeq = {
 
     import resourceEntry._
 
     val objectURIstringValue = value.toString()
     val css = cssForURI(objectURIstringValue)
 
-    /* provide draggable hyperlinks to form's fields, suitable to drop in social media */
-    val hyperlinkToField = {
-      val id = urlEncode(resourceEntry.property).replace("%", "-")
-      /*  ID and NAME tokens must begin with a letter ([A-Za-z]) and may be followed by any number of letters, 
-       *  digits ([0-9]), hyphens ("-"), underscores ("_"), colons (":"), and periods ("."). */
-      if( objectURIstringValue != "" ) {
+
+    // TODO Text("\n") should be within specific val's
+    hyperlinkToField(resourceEntry, value.toString) ++
+      //hyperlinkToObjectURI ++ Text("\n") ++
+      hyperlinkToObjectURI(hrefDisplayPrefix, objectURIstringValue, valueLabel, type_, resourceEntry) ++
+      backLinkButton (objectURIstringValue, label, value) ++
+      normalNavigationButton(objectURIstringValue, value) ++
+      makeDrawGraphLink(objectURIstringValue) ++
+      //      makeDrawGraphLink(objectURIstringValue,
+      //          toolURLprefix=
+      //            s"https://scenaristeur.github.io/graphe/?endpoint=${request.localSparqlEndpoint}" +
+      //            s"&sujet=",
+      //            toolname="scenaristeur/graphe"
+      //            ,
+      //            imgWidth=6
+      //          ) ++ Text("\n") ++
+      thumbnail_(resourceEntry, isImage, value, valueLabel) ++
+      {makeUserInfoOnTriples(resourceEntry.metadata,resourceEntry.timeMetadata)}
+  }
+
+  def hyperlinkToField(resourceEntry: formMod#ResourceEntry, objectURIstringValue: String) = {
+    val id = urlEncode(resourceEntry.property).replace("%", "-")
+    /*  ID and NAME tokens must begin with a letter ([A-Za-z]) and may be followed by any number of letters,
+     *  digits ([0-9]), hyphens ("-"), underscores ("_"), colons (":"), and periods ("."). */
+    if( objectURIstringValue != "" ) {
       <a href={ "#" + id } draggable="true">
-      <i class="glyphicon glyphicon-link"></i>
+        <i class="glyphicon glyphicon-link"></i>
       </a>
-      <a id={ id }></a>
-      } else NodeSeq.Empty
-    }
+        <a id={ id }></a>
+    } else NodeSeq.Empty
+  }
 
-    //    val atts = new UnprefixedAttribute("", )
+  def hyperlinkToObjectURI(hrefPrefix: String, objectURIstringValue: String, valueLabel: String, type_ : NODE, resourceEntry: formMod#ResourceEntry) = {
+    addTripleAttributesToXMLElement(
+      <a href={createHyperlinkString(hrefPrefix, objectURIstringValue)} class={cssForURI(objectURIstringValue)} title=
+      {s"""Value ${if (objectURIstringValue != valueLabel) objectURIstringValue else ""}
+              of type ${type_.toString()}"""} draggable="true">
+        {valueLabel}
+      </a>,
+      resourceEntry)
+  }
 
-    val hyperlinkToObjectURI =
-      addTripleAttributesToXMLElement(
-        <a href={ createHyperlinkString(hrefPrefix, objectURIstringValue) } class={ css } title={
-          s"""Value ${if (objectURIstringValue != valueLabel) objectURIstringValue else ""}
-              of type ${type_.toString()}"""
-        } draggable="true" >{
-          valueLabel
-        }</a>,
-        resourceEntry)
+  def backLinkButton(objectURIstringValue: String, label: String, value: NODE) = {
+    (if (objectURIstringValue.size > 0 && showExpertButtons) {
+      val title = s""" Reverse links for "$label" "$value" """
+      makeBackLinkButton(objectURIstringValue, title = title)
+    } else NodeSeq.Empty)
+  }
 
-    val backLinkButton = (if (objectURIstringValue.size > 0 && showExpertButtons) {
-				val title = s""" Reverse links for "$label" "$value" """
-				makeBackLinkButton(objectURIstringValue, title=title )
-      } else NodeSeq.Empty )
+  def normalNavigationButton(objectURIstringValue: String, value: NODE) = {
+    (if (objectURIstringValue.size > 0 && showExpertButtons) {
+      <a class="btn btn-primary" href={objectURIstringValue} title={s"Normal HTTP link to $value"}
+         draggable="true">
+        <i class="glyphicon glyphicon-share-alt"></i>
+      </a>
+    } else NodeSeq.Empty)
+  }
 
-    val normalNavigationButton = (if (objectURIstringValue.size > 0 && showExpertButtons) {
-      <a class="btn btn-primary" href={ objectURIstringValue } title={ s"Normal HTTP link to $value" }
-      draggable="true"><i class="glyphicon glyphicon-share-alt"></i> </a>
-    } else NodeSeq.Empty )
 
-    val thumbnail = {
+  def thumbnail_(resourceEntry: formMod#ResourceEntry, isImage: Boolean, value: NODE, valueLabel: String) = {
+    {
       val thumbnail = resourceEntry.thumbnail
       val imageURL = if (isImage) Some(value)
       else thumbnail
-      if (isImage || thumbnail.isDefined){
-        <a class="image-popup-vertical-fit" href={ imageURL.get.toString() } title={s"Image of $valueLabel: ${value.toString()}"}>
-          <img src={ imageURL.get.toString() } css="sf-thumbnail" height="40" alt={
-          s"Image of $valueLabel: ${value.toString()}"
-        }/></a>
+      if (isImage || thumbnail.isDefined) {
+        <a class="image-popup-vertical-fit" href={imageURL.get.toString()} title={s"Image of $valueLabel: ${value.toString()}"}>
+          <img src={imageURL.get.toString()} css="sf-thumbnail" height="40" alt={s"Image of $valueLabel: ${value.toString()}"}/>
+        </a>
       }
       else NodeSeq.Empty
-      }
-
-    // TODO Text("\n") should be within specific val's
-    hyperlinkToField ++
-    hyperlinkToObjectURI ++ Text("\n") ++
-    backLinkButton ++ Text("\n") ++
-    normalNavigationButton ++ Text("\n") ++
-    makeDrawGraphLink(objectURIstringValue) ++
-//      makeDrawGraphLink(objectURIstringValue,
-//          toolURLprefix=
-//            s"https://scenaristeur.github.io/graphe/?endpoint=${request.localSparqlEndpoint}" +
-//            s"&sujet=",
-//            toolname="scenaristeur/graphe"
-//            ,
-//            imgWidth=6
-//          ) ++ Text("\n") ++
-      thumbnail ++
-      {makeUserInfoOnTriples(resourceEntry.metadata,resourceEntry.timeMetadata)}
+    }
   }
+
 
   private def makeUserInfoOnTriples(userMetadata: String,timeMetadata: Long) ={
     val time :String = new DateTime(timeMetadata).toDateTime.toString("dd/MM/yyyy HH:mm")
@@ -116,11 +126,11 @@ trait Form2HTMLDisplay[NODE, URI <: NODE]
   }
 
   private[html] def createHTMLBlankNodeReadonlyField(
-    r: formMod#BlankNodeEntry,
-    hrefPrefix: String = config.hrefDisplayPrefix) =
+                                                      r: formMod#BlankNodeEntry,
+                                                      hrefPrefix: String = config.hrefDisplayPrefix) =
     <a href={ Form2HTML.createHyperlinkString(hrefPrefix, r.value.toString, true) }>{
       r.valueLabel
-    }</a> ++
-        {makeUserInfoOnTriples(r.metadata,r.timeMetadata)}
+      }</a> ++
+      {makeUserInfoOnTriples(r.metadata,r.timeMetadata)}
 
 }
