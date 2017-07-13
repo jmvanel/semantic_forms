6a21e39525d1e0dfe6260fca1fc9f551f807139b
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-07-13 11:26:17 +0200

    compute path length from geo data: load statistics in SF - for Mobion
    TODO
    - document SPARQL query for getting statitics
    - distinct services for showing path lengths and computing them
    - provide landing web page with relevant URL's for displaying geolocalization of vehicles
    - display RDF data as report

--- scala/forms/src/main/scala/deductions/runtime/services/GeoController.scala
+++ scala/forms/src/main/scala/deductions/runtime/services/GeoController.scala
@@ -1,9 +1,11 @@
 package deductions.runtime.services
 
-import deductions.runtime.abstract_syntax.{FormSyntaxFactory, FormSyntaxFromSPARQL}
+import deductions.runtime.abstract_syntax.{ FormSyntaxFactory, FormSyntaxFromSPARQL }
+import deductions.runtime.sparql_cache.dataset.RDFStoreLocalProvider
 import deductions.runtime.html.TableView
 import deductions.runtime.sparql_cache.algos.GeoPath
 import deductions.runtime.utils.HTTPrequest
+
 import org.w3.banana.RDF
 
 import scala.xml.NodeSeq
@@ -13,15 +15,12 @@ trait GeoController[Rdf <: RDF, DATASET] extends GeoPath[Rdf, DATASET]
     with SemanticController
     with FormSyntaxFactory[Rdf, DATASET]
     with FormSyntaxFromSPARQL[Rdf, DATASET]
-    with TableView[Rdf#Node, Rdf#URI] {
+    with TableView[Rdf#Node, Rdf#URI]
+    with RDFStoreLocalProvider[Rdf, DATASET] {
 
   import ops._
 
-  def result(request: HTTPrequest): NodeSeq = {
-    val res = wrapInReadTransaction {
-      val statisticsGraph = getPathLengthForAllMobiles(allNamedGraph)
-      println(s"statisticsGraph size ${ops.graphSize(statisticsGraph)}")
-      val detailsQuery = """
+  val detailsQuery = """
         |${declarePrefix("geoloc")}
         |${declarePrefix("vehman")}
         |CONSTRUCT {
@@ -35,17 +34,36 @@ trait GeoController[Rdf <: RDF, DATASET] extends GeoPath[Rdf, DATASET]
         |    ?VEHICULE vehman:internalNumber ?NUM.
         |  }
         |}""".stripMargin
-      val detailsGraph = sparqlConstructQuery( detailsQuery ) . getOrElse(ops.emptyGraph)
 
+  def result(request: HTTPrequest): NodeSeq = {
+
+    val statisticsGraphTry = wrapInReadTransaction {
+      getPathLengthForAllMobiles(allNamedGraph)
+    }
+
+    val res = for (
+      statisticsGraph <- statisticsGraphTry;
+      detailsGraph <- wrapInReadTransaction {
+        sparqlConstructQuery(detailsQuery).getOrElse(ops.emptyGraph)
+      }
+    ) yield {
+      println(s"statisticsGraph size ${ops.graphSize(statisticsGraph)}")
       // create table view
       val formSyntax = createFormFromTriples(
         getTriples(
-            union( Seq(statisticsGraph, detailsGraph))
-        ).toSeq,
+          union(Seq(statisticsGraph, detailsGraph))).toSeq,
         false)(allNamedGraph, "en")
       generate(formSyntax)
     }
-    val table = res.getOrElse(<div>Error! {res}</div>)
+
+    // load statistics in SF
+    val res2 = wrapInTransaction {
+      for (statisticsGraph <- statisticsGraphTry) {
+        rdfStore.appendToGraph(dataset, URI("geoloc:stats"), statisticsGraph)
+      }
+    }
+
+    val table = res.getOrElse(<div>Error! { res }</div>)
     table
   }
 }
