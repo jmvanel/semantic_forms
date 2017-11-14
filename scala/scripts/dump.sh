c8936a8bc9b35040f39bc601c15a1608ded139b4
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-11-14 17:49:22 +0100

    scripts/dump.sh : apply sed commands to remove bad SBT characters

--- scala/scripts/dump.sh
+++ scala/scripts/dump.sh
@@ -2,6 +2,6 @@
 
 echo runMain tdb.tdbdump --loc=TDB  EOF \> dump.nq
 
-sbt "project forms_play" "runMain tdb.tdbdump --loc=TDB" > dump.nq
+sbt "project forms_play" "runMain tdb.tdbdump --loc=TDB" | sed 's/^.\{,23\}// ; s/....$//' > dump.nq
 ls -l dump.nq
 echo DONE
