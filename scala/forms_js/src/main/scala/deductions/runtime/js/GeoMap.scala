9c6d1d0504c2e9b0d88c39d72fd222616b122f10
jmvanel <jeanmarc.vanel@gmail.com>
2017-12-05 16:09:37 +0100

    sketch of Geographic Map in Scala.JS

---
+++ scala/forms_js/src/main/scala/deductions/runtime/js/GeoMap.scala
@@ -0,0 +1,14 @@
+package deductions.runtime.js
+
+import scala.scalajs.js
+
+/*** get RDF data from URL and display an OSM map
+ * using OSM tiles and RDF-ext
+ * 
+ * see RDF-ext example: http://rawgit.com/rdf-ext/rdf-examples/develop/parse-jsonld-to-dataset.html
+ * NOTE: RDF-ext has no Scala.js JavaScript facade,
+ * see doc. for JavaScript facades
+ * https://www.scala-js.org/doc/interoperability/facade-types.html */
+trait GeoMap {
+  
+}
