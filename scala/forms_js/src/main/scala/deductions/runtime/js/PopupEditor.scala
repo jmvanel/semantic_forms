a09516901c22a02e2e1fc088e61d76e8dc2e900e
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2017-12-18 19:14:23 +0100

    REFACTOR PopupEditor.scala, add forms_play/public/editor-pen.html (only (?) way to add stuf to page <head> )

--- scala/forms_js/src/main/scala/deductions/runtime/js/PopupEditor.scala
+++ scala/forms_js/src/main/scala/deductions/runtime/js/PopupEditor.scala
@@ -7,6 +7,7 @@ import scala.scalajs.js.JSApp
 import scala.scalajs.js
 import js.Dynamic.literal
 import org.scalajs.dom.raw.BeforeUnloadEvent
+import org.scalajs.dom.raw.HTMLDocument
 
 // @JSExport
 /** outdated, currently we use the JavaScript in forms_play/public/ */
@@ -19,46 +20,13 @@ object PopupEditor extends JSApp {
   def launchEditorWindow(input: html.Input): Unit = {
 
     // setup new window
-    val popupWindow = dom.window.open("",
+    val popupWindow = dom.window.open("/assets/editor-pen.html",
       "Edit_Markdown_text_for_semantic_forms",
-      "height=500,width=500,resizable=yes,modal=yes")
-    val closeButton = makeButton(popupWindow, "SAVE")
-    val exitButton = makeButton(popupWindow, "DISMISS")
+      "height=500,width=500,resizable=yes,modal=yes")    
+    val popupDocument = popupWindow.document
 
-    val editorDiv = popupWindow.document.createElement("div")
-    val body = popupWindow.document.body
-    body.appendChild(closeButton)
-    body.appendChild(exitButton)
-    body.appendChild(editorDiv)
-
-    // launch Pen editor
-    val child = popupWindow.document.createElement("script")
-    val typ = popupWindow.document.createAttribute("type")
-      typ.value = "text/javascript"
-      child.attributes.setNamedItem(typ)
-    val src = popupWindow.document.createAttribute("src")
-      src.value = "https://rawgit.com/sofish/pen/master/src/markdown.js"
-      child.attributes.setNamedItem(src)
-    popupWindow.document.head.appendChild( child )
-
-    /* <script type="text/javascript" async="true"
-     * src="https://rawgit.com/sofish/pen/master/src/markdown.js"></script> */
-
-    val options = literal(
-      "editor" -> editorDiv,
-      "class" -> "pen",
-      "debug" -> true,
-      "list" -> // editor menu list
-        js.Array("insertimage", "blockquote", "h2", "h3", "p", "code",
-          "insertorderedlist", "insertunorderedlist", "inserthorizontalrule",
-          "indent", "outdent", "bold", "italic", "underline", "createlink")
-    )
-    val pen = js.Dynamic.global.Pen
-    val editor = js.Dynamic.newInstance(pen)(options)
-
-    editorDiv.innerHTML = input.value
-    if (input.value == "")
-      editorDiv.innerHTML = "?"
+    val (editorDiv, body, closeButton, exitButton) = addButtonsToWindow(popupDocument)
+    val editor = launchPenEditor(editorDiv, input)
 
     // to avoid message "data you have entered may not be saved."
     dom.window.onbeforeunload = (_: BeforeUnloadEvent) => {
@@ -86,8 +54,50 @@ object PopupEditor extends JSApp {
     exitButton.onclick = (_: dom.MouseEvent) => popupWindow.close()
   }
 
-  def makeButton(window: org.scalajs.dom.raw.Window, label: String): html.Button = {
-    val button = window.document.createElement("button").
+  /** launch Pen editor */
+  private def launchPenEditor(editorDiv: org.scalajs.dom.raw.Element, input: org.scalajs.dom.html.Input) = {// launch Pen editor
+
+    val options = literal(
+      "editor" -> editorDiv,
+      "class" -> "pen",
+      "debug" -> true,
+      "list" -> // editor menu list
+        js.Array("insertimage", "blockquote", "h2", "h3", "p", "code",
+          "insertorderedlist", "insertunorderedlist", "inserthorizontalrule",
+          "indent", "outdent", "bold", "italic", "underline", "createlink")
+    )
+    val pen = js.Dynamic.global.Pen
+    val editor = js.Dynamic.newInstance(pen)(options)
+
+    editorDiv.innerHTML = input.value
+    if (input.value == "")
+      editorDiv.innerHTML = "?"
+    editor
+  }
+
+  private def addButtonsToWindow(popupDocument: org.scalajs.dom.raw.HTMLDocument) = {//    val child = popupDocument.createElement("script")
+//    val typ = popupDocument.createAttribute("type")
+//      typ.value = "text/javascript"
+//      child.attributes.setNamedItem(typ)
+//    val src = popupDocument.createAttribute("src")
+//      src.value = "https://rawgit.com/sofish/pen/master/src/markdown.js"
+//      child.attributes.setNamedItem(src)
+//    popupDocument.head.appendChild( child )
+
+    val closeButton = makeButton(popupDocument, "SAVE")
+    val exitButton = makeButton(popupDocument, "DISMISS")
+    val editorDiv = popupDocument.createElement("div")
+
+    val body = popupDocument.body
+    body.appendChild(closeButton)
+    body.appendChild(exitButton)
+    body.appendChild(editorDiv)
+    (editorDiv, body, closeButton, exitButton)
+  }
+
+  private def makeButton(document: HTMLDocument,
+      label: String): html.Button = {
+    val button = document.createElement("button").
       asInstanceOf[html.Button]
     button.innerHTML = label
     button.textContent = label
