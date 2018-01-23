11908efc34164a563220be883546639d13f5afca
jmvanel <jeanmarc.vanel@gmail.com>
2018-01-23 19:25:48 +0100

    scalawebtest: add basic test for landing page

--- scala/scalawebtest/src/test/scala/deductions/runtime/scalawebtest/index.scala
+++ scala/scalawebtest/src/test/scala/deductions/runtime/scalawebtest/index.scala
@@ -1,10 +1,13 @@
 import org.scalawebtest.core.IntegrationFlatSpec
+import org.scalawebtest.core.gauge.HtmlGauge
 
-                    class HomepageSpec extends IntegrationFlatSpec {
-                        override val host = "http://www.scalawebtest.org"
-                        path = "/index.html"
+class HomepageSpec extends IntegrationFlatSpec with HtmlGauge {
+    override val host = "http://localhost:9000"
+    path = ""
 
-                        "Our homepage" should "contain a succinct claim" in {
-                            webDriver.findElementByTagName("h2").getText shouldEqual "Reduce the effort needed to write integration tests"
-                        }
-                    }
+    "Our homepage" should "contain a succinct claim" in {
+    fits(
+            <h1> Welcome to Semantic forms </h1>
+    )
+  }
+}
