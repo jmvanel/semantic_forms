9fa09799f25e7b0e37773261ec5ed24ee07e2f26
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2018-03-06 11:14:21 +0100

    TESTED r-ldp-post-directory.sh with:
    ~/src/semantic_forms/scala/forms_play/dist/scripts/r-ldp-post-directory.sh \
      ~/data/yannick/cartopair/ \
      http://localhost:9000/ldp/yannnn1 \
      text/turtle

--- scala/forms_play/dist/scripts/r-ldp-post-directory.sh
+++ scala/forms_play/dist/scripts/r-ldp-post-directory.sh
@@ -3,7 +3,7 @@ LDP_SERVICE=$2
 
 # default value for arg. 3, Content-Type
 if [ -z "$2" ]; then
-  echo 'Arguments: RDF_DIR LDP_SERVICE [MIME]'
+  echo 'Arguments: RDF_DIR LDP_SERVICE(full) [MIME]'
   echo 'Content-Type [MIME] : application/ld+json application/rdf+xml text/turtle'
   exit
 fi
@@ -17,27 +17,33 @@ fi
 
 # LDP PUT
 function put {
-  RELATIVE_PATH=$1
-  file=`filename $RELATIVE_PATH`
-  RELATIVE_DIR=`dirname $RELATIVE_PATH`
+  GIVEN_PATH=$1
+  GIVEN_DIR=$2
+  file=`filename $GIVEN_PATH`
+  GIVEN_DIR_PARENT=`dirname $GIVEN_DIR`
+  GIVEN_PATH_PARENT=`dirname $GIVEN_PATH`
+  REL_PATH=`echo $GIVEN_PATH_PARENT | sed 's|'$GIVEN_DIR_PARENT'/||'`  
+  echo GIVEN_PATH $GIVEN_PATH, GIVEN_DIR $GIVEN_DIR, GIVEN_DIR_PARENT $GIVEN_DIR_PARENT, GIVEN_PATH_PARENT $GIVEN_PATH_PARENT, REL_PATH $REL_PATH
+  RELATIVE_DIR=$REL_PATH
   echo wget --output-file=FILE \
-     --save-headers --post-file=$RDF_DIR/RELATIVE_DIR \
-     "$LDP_SERVICE \
+     --save-headers --post-file=$GIVEN_PATH \
+     $LDP_SERVICE/$RELATIVE_DIR \
      --header="Content-Type: $MIME" \
      --header="Slug: $file"
   wget --output-file=FILE \
-     --save-headers --post-file=$RDF_DIR/RELATIVE_DIR \
-     "$LDP_SERVICE \
+     --save-headers --post-file=$GIVEN_PATH \
+     $LDP_SERVICE/$RELATIVE_DIR \
      --header="Content-Type: $MIME" \
      --header="Slug: $file"
 
-  echo body-file=$RDF_DIR/RELATIVE_PATH sent to server $LDP_SERVICE 
-  echo result saved in FILE "FILE"
+  echo file=$RDF_DIR/GIVEN_PATH sent to server $LDP_SERVICE 
+  echo result saved in FILE "FILE" :
   cat FILE
 }
 
 # for f in $RDF_DIR/*/*
-for f in $RDF_DIR/*
+# for f in $RDF_DIR/*
+for f in `find $RDF_DIR -type f `
 do
-  put $f
+  put $f $RDF_DIR
 done
