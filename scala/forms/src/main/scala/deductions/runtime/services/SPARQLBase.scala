15cd3280c10cbfed4d6f85d5c3dcdad993515f78
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2018-04-25 09:50:02 +0200

    FIX /search ( bad SPARQL )

--- scala/forms/src/main/scala/deductions/runtime/services/SPARQLBase.scala
+++ scala/forms/src/main/scala/deductions/runtime/services/SPARQLBase.scala
@@ -10,11 +10,7 @@ trait SPARQLBase {
 
   private val excludePersonOLD = """  FILTER( NOT EXISTS { ?thing a dbo:Person } )"""
   
-  val excludePerson = """  FILTER( NOT EXISTS {
-       ?thing a ?sub .
-       ?sub rdfs:subClassOf* dbo:Person } )"""
-
-    val excludePlace = """  FILTER( NOT EXISTS {
-       ?thing a ?sub .
-       ?sub rdfs:subClassOf* dbo:Place } )"""
+  /** NOTE designed for <urn:x-arq:UnionGraph> */
+  val excludePerson = """  FILTER( NOT EXISTS { ?thing a ?sub . ?sub rdfs:subClassOf* dbo:Person } )"""
+  val excludePlace  = """  FILTER( NOT EXISTS { ?thing a ?sub . ?sub rdfs:subClassOf* dbo:Place } )"""
 }
