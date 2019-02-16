73904f481c18f3921e94f48e9a0ec6a5a005228e
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2019-02-16 22:24:20 +0100

    RDF to iCalendar converter as a command line App #203

---
+++ scala/connectors/src/main/scala/deductions/runtime/connectors/icalendar/RDF2ICalendar.scala
@@ -0,0 +1,103 @@
+package deductions.runtime.connectors.icalendar
+
+import org.w3.banana.RDF
+import org.w3.banana.RDFOps
+import deductions.runtime.utils.RDFPrefixes
+import deductions.runtime.utils.RDFHelpers
+
+/** RDF graph to ICalendar converter
+ *  see https://en.wikipedia.org/wiki/ICalendar
+ *  */
+trait RDF2ICalendar[Rdf <: RDF] extends RDFPrefixes[Rdf] with RDFHelpers[Rdf] {
+  implicit val ops: RDFOps[Rdf]
+  import ops._
+
+  /** main reusable function in API */
+  def graph2iCalendar(graph: Rdf#Graph): String = {
+
+    val eventTriples = find(graph, ANY, rdf.typ, schema("Event"))
+    val events = new StringBuilder()
+
+    def addLine(s: String): StringBuilder = {
+      if (s != "") {
+        events append s
+        events append '\n'
+      }
+      return events
+    }
+
+    def processSubject( subject: Rdf#Node) = {
+      addLine ("BEGIN:VEVENT")
+      addLine( "UID:" + subject )
+      for (
+          triple2 <- find(graph, subject, ANY, ANY);
+          _ = processTriple(triple2)
+      ){}
+      addLine( "END:VEVENT" )
+    }
+
+    def getDisplayLabel(objet: Rdf#Node) =
+      nodeToString(getObjects(graph, objet, URI("urn:displayLabel")).headOption.getOrElse(Literal("")))
+
+    def processTriple(triple: Rdf#Triple) = {
+      val pred = triple.predicate;
+      val objet = triple.objectt
+      addLine( pred match {
+        case dbo("startDate") => "DTSTAMP:" + nodeToString(objet).replaceAll("-", "")
+        case URI("http://purl.org/NET/c4dm/event.owl#agent") =>
+          "ORGANIZER:" + getDisplayLabel(objet)
+        case dbo("startDate") => "DTSTART:" + nodeToString(objet).replaceAll("-", "")
+        case dbo("endDate")   => "DTEND:" + nodeToString(objet).replaceAll("-", "")
+        case URI("urn:displayLabel")   => "SUMMARY:" + nodeToString(objet)
+        case URI("http://purl.org/NET/c4dm/event.owl#place") =>
+                                 "GEO:" + getDisplayLabel(objet)
+        case rdfs.comment     => "DESCRIPTION:" + nodeToString(objet)
+        case _                => ""
+      })
+    }
+
+    for ( triple <- eventTriples) {
+        processSubject(triple.subject);
+    }
+
+    return """BEGIN:VCALENDAR
+      |VERSION:2.0""".stripMargin +
+      events.toString() +
+      "END:VCALENDAR"
+  }
+  
+/* Output:
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:uid1@example.com
+DTSTAMP:19970714T170000Z
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+SUMMARY:Bastille Day Party
+GEO:48.85299;2.36885
+END:VEVENT
+END:VCALENDAR
+Input:
+<http://gmbvs.fr/events/row/26>
+        a                   <http://dbpedia.org/ontology/Event> , <http://schema.org/Event> ;
+        <http://www.w3.org/2000/01/rdf-schema#comment>
+                "Vendredi 4 samedi 5 dimanche 6 octobre 2019"@fr ;
+        <http://www.w3.org/2000/01/rdf-schema#label>
+                "Week-end mycologique à Cerniébeau (39250)" ;
+        <http://dbpedia.org/ontology/endDate>
+                "2019-10-06"^^<http://www.w3.org/2001/XMLSchema#date> ;
+        <http://dbpedia.org/ontology/startDate>
+                "2019-10-04"^^<http://www.w3.org/2001/XMLSchema#date> ;
+        <http://deductions.github.io/event.form.ttl#time>
+                "" ;
+        <http://purl.org/NET/c4dm/event.owl#agent>
+                <http://gmvs69.free.fr/> ;
+        <http://purl.org/NET/c4dm/event.owl#place>
+                <http://dbpedia.org/resource/Cerniébeau> ;
+        <http://purl.org/dc/terms/subject>
+                <http://dbpedia.org/resource/Mycology>
+ */
+}
---
+++ scala/forms/src/main/scala/deductions/runtime/connectors/RDF2ICalendarApp.scala
@@ -0,0 +1,30 @@
+package deductions.runtime.connectors
+
+import java.io.FileOutputStream
+import deductions.runtime.connectors.icalendar.RDF2ICalendar
+import deductions.runtime.jena.ImplementationSettings
+import deductions.runtime.utils.DefaultConfiguration
+
+/** simple App that takes all triples and formats the schema:Event in ICalendar format */
+object RDF2ICalendarApp
+  extends {
+    override val config = new DefaultConfiguration {
+      override val useTextQuery: Boolean = true // false
+    }
+  } with App
+  with ImplementationSettings.RDFCache
+  with RDF2ICalendar[ImplementationSettings.Rdf] {
+
+  writeGraph(allNamedGraph)
+
+  def writeGraph(graph: Rdf#Graph) {
+    val outputFile = "from-RDF.ics"
+    println(s"""Write $outputFile,
+    # of triples ${graph.size()}""")
+    val os = new FileOutputStream(outputFile)
+    val iCalendar = graph2iCalendar(graph)
+    os.write(iCalendar.getBytes)
+    os.close()
+    println(s"Writen $outputFile" )
+  }
+}
