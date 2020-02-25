9013bc00424fcbeb2f3a59adacfa8c317d80155f
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-02-25 09:40:34 +0100

    FEATURE: Proxy for HTTP get; will be used for HTTP images that need to be HTTPS; same for dbPedia Lookup
    TESTED with http://localhost:9000/proxy?originalurl=http://jmvanel.free.fr/images/jmv_id.jpg

--- scala/forms_play/app/controllers/Services.scala
+++ scala/forms_play/app/controllers/Services.scala
@@ -385,4 +385,55 @@ with RDFContentNegociation {
       Ok(void).as("text/turtle; charset=utf-8")
     }
   }
+
+  /**
+   * Returns the text (content) from a REST URL as a String.
+   * Inspired by http://matthewkwong.blogspot.com/2009/09/scala-scalaiosource-fromurl-blockshangs.html
+   * and http://alvinalexander.com/blog/post/java/how-open-url-read-contents-httpurl-connection-java
+   *
+   * The `connectTimeout` and `readTimeout` comes from the Java URLConnection
+   * class Javadoc.
+   * @param url The full URL to connect to.
+   * @param connectTimeout Sets a specified timeout value, in milliseconds,
+   * to be used when opening a communications link to the resource referenced
+   * by this URLConnection. If the timeout expires before the connection can
+   * be established, a java.net.SocketTimeoutException
+   * is raised. A timeout of zero is interpreted as an infinite timeout.
+   * Defaults to 5000 ms.
+   * @param readTimeout If the timeout expires before there is data available
+   * for read, a java.net.SocketTimeoutException is raised. A timeout of zero
+   * is interpreted as an infinite timeout. Defaults to 5000 ms.
+   * @param requestMethod Defaults to "GET". (Other methods have not been tested.)
+   *
+   * @example get("http://www.example.com/getInfo")
+   * @example get("http://www.example.com/getInfo", 5000)
+   * @example get("http://www.example.com/getInfo", 5000, 5000)
+   */
+  @throws(classOf[java.io.IOException])
+  @throws(classOf[java.net.SocketTimeoutException])
+  private def get(
+    url:            String,
+    connectTimeout: Int    = 5000,
+    readTimeout:    Int    = 5000,
+    requestMethod:  String = "GET") =
+  {
+    import java.net.{ URL, HttpURLConnection }
+    val connection = (new URL(url)).openConnection.asInstanceOf[HttpURLConnection]
+    connection.setConnectTimeout(connectTimeout)
+    connection.setReadTimeout(readTimeout)
+    connection.setRequestMethod(requestMethod)
+    val inputStream = connection.getInputStream
+    val byteArray = org.apache.commons.io.IOUtils.toByteArray(inputStream)
+    if (inputStream != null) inputStream.close
+    (byteArray, connection)
+  }
+
+  /** Proxy for HTTP get; used for HTTP images that need to be HTTPS;
+   *  same for dbPedia Lookup */
+  def getProxy(originalurl: String) = Action {
+    implicit request =>
+      val (content, connection) = get(originalurl)
+      Ok(content).as(connection.getContentType)
+  }
+
 }
