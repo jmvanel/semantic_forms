b97ea1f13946cb4e66ca94ca11362a0ce5fa2e4b
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-06-02 12:41:25 +0200

    fallback-server: Web Server for maintenance times, based on akka-http
    - TODO harmonize versions of akka-http & akka-stream with the rest of SF

---
+++ scala/fallback-server/src/main/scala/WebServer.scala
@@ -0,0 +1,42 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
+
+/** Web Server for maintenance times.
+ *  see https://doc.akka.io/docs/akka-http/current/introduction.html */
+object WebServer {
+  def main(args: Array[String]) {
+
+    val port = args .lift(0).getOrElse("8080").toInt
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+            """
+            <h1>Ce site est en maintenance</h1>
+            Soyez patients...
+
+           <h1>This site is in maintenance</h1>
+            Be patient...      
+            """))
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", port)
+
+    println(s"Server online at http://localhost:$port/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
