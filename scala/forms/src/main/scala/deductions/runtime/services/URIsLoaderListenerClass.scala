425936351c22d47c5ba95b88fdc3443c6cd5ad97
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-06-13 21:17:46 +0200

    FIX: in a listener for /save , was trying to load a literal from Internet !
    - this created big log when the literal is big

--- scala/forms/src/main/scala/deductions/runtime/services/URIsLoaderListenerClass.scala
+++ scala/forms/src/main/scala/deductions/runtime/services/URIsLoaderListenerClass.scala
@@ -8,12 +8,14 @@ import deductions.runtime.utils.SaveListener
 import deductions.runtime.sparql_cache.RDFCacheAlgo
 import org.w3.banana.RDF
 import scala.concurrent.Future
+import deductions.runtime.utils.RDFHelpers
 
 /** URI's Loader Listener, for form submit */
 class URIsLoaderListenerClass(val config: Configuration)
   extends URIsLoaderListenerTrait[ImplementationSettings.Rdf, ImplementationSettings.DATASET]
     with ImplementationSettings.RDFModule
     with ImplementationSettings.RDFCache
+    with RDFHelpers[ImplementationSettings.Rdf]
 
 trait URIsLoaderListenerTrait[Rdf <: RDF, DATASET]
   extends SaveListener[Rdf]
@@ -32,13 +34,18 @@ trait URIsLoaderListenerTrait[Rdf <: RDF, DATASET]
     import scala.concurrent.ExecutionContext.Implicits.global
     for (tr <- addedTriples) {
       val uri = tr.objectt
-      Future {
-        retrieveURIBody(
-          forceNodeToURI(uri), dataset,
-          request,
-          transactionsInside = true)
-        logger.info(s"URIsLoaderListenerTrait: $uri was loaded.")
+      if (isURI(uri)) {
+        Future {
+          retrieveURIBody(
+            forceNodeToURI(uri), dataset,
+            request,
+            transactionsInside = true)
+          logger.info(s"URIsLoaderListenerTrait: <$uri> was loaded (when subject <${tr.subject}> was updated).")
+        }
       }
+//      else {
+//        println(s"URIsLoaderListenerTrait notifyDataEvent: NOT an URI : $uri")
+//      }
     }
   }
 }
