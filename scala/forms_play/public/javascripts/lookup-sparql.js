0271e6e2451bb892f44c8a691d958026ecc72549
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-07-11 09:45:38 +0200

    REFACTOR: non-URL encoded SPARQL in lookup-sparql.js
    - more readable JS code by using a Template literal for the SPARQL
    - tried completion on vernacular Name too, but waiting for indexation on
    the TAXREF server

--- scala/forms_play/public/javascripts/lookup-sparql.js
+++ scala/forms_play/public/javascripts/lookup-sparql.js
@@ -20,7 +20,8 @@ https://www.google.fr/search?q=ajax+example+scala.js
 
 $(document).ready(function() {
   const resultsCount = 15
-  const lookupServer = "http://sparks-vm33.i3s.unice.fr:8890/sparql" // used for printing log
+  const lookupServer = "https://taxref.i3s.unice.fr"
+		// "http://sparks-vm33.i3s.unice.fr:8890/sparql"
 
   const lookupCSSclass = '.virtuosoLookup'
   const suggestionSearchCSSclass = 'sf-suggestion-search-dbpedia'
@@ -41,16 +42,36 @@ function(searchServiceURL, request, inputElement, callback, getRDFtypeInURL){
   if( words . length > 1 )
     stringToSearch = words[0] + '*_' +  words[1] + '*'
   else
-    stringToSearch = stringToSearch + "*"
+    stringToSearch = `"${stringToSearch}*"`
   console.log( "stringToSearch '" + stringToSearch + "'" )
+
   return (
     $.ajax({
-      url: // makeProxiedLookupURL(
-	   // "http://sparks-vm33.i3s.unice.fr:8890
-	   "https://taxref.i3s.unice.fr" +
-	   "/sparql?default-graph-uri=&query=+select+%3Fs1+as+%3Fc1%2C+%28+bif%3Asearch_excerpt+%28+bif%3Avector+%28+%27TINCTORIA%27%2C+%27GENISTA%27+%29+%2C+%3Fo1+%29+%29+as+%3Fc2%2C+%3Fsc%2C+%3Frank%2C+%3Fg+where+%0D%0A++%7B+%0D%0A++++%7B+%0D%0A++++++%7B+%0D%0A++++++++select+%3Fs1%2C+%28+%3Fsc+*+3e-1+%29+as+%3Fsc%2C+%3Fo1%2C+%28+sql%3Arnk_scale+%28+%3CLONG%3A%3AIRI_RANK%3E+%28+%3Fs1+%29+%29+%29+as+%3Frank%2C+%3Fg+where+%0D%0A++++++++%7B+%0D%0A++++++++++quad+map+virtrdf%3ADefaultQuadMap+%0D%0A++++++++++%7B+%0D%0A++++++++++++graph+%3Chttp%3A%2F%2Ftaxref.mnhn.fr%2Flod%2Fgraph%2Fclasses%2F13.0%3E+%0D%0A++++++++++++%7B+%0D%0A++++++++++++++%3Fs1+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23label%3E+%3Fo1+.%0D%0A++++++++++++++%3Fo1+bif%3Acontains+%27+%28+" +
-    '%22' + stringToSearch + '%22' +// " TINCTORIA+AND+GENISTA "
-    "+%29+%27+option+%28+score+%3Fsc+%29+.%0D%0A++++++++++++++%0D%0A++++++++++++%7D%0D%0A+++++++++++%7D%0D%0A+++++++++%7D%0D%0A+++++++order+by+desc+%28+%3Fsc+*+3e-1+%2B+sql%3Arnk_scale+%28+%3CLONG%3A%3AIRI_RANK%3E+%28+%3Fs1+%29+%29+%29+limit+20+offset+0+%0D%0A++++++%7D%0D%0A+++++%7D%0D%0A+++%7D%0D%0A++&format=application%2Fsparql-results%2Bjson&timeout=0" , // ) ,
+      url: searchServiceURL +
+	   "/sparql?default-graph-uri=&query=" +
+	    encodeURIComponent(
+	    `select ?s1 as ?c1, ( bif:search_excerpt ( bif:vector () , ?o1 ) ) as ?c2, ?sc, ?rank, ?g where {
+        select ?s1, ( ?sc * 3e-1 ) as ?sc, ?o1, ( sql:rnk_scale ( <LONG::IRI_RANK> ( ?s1 ) ) ) as ?rank, ?g where
+        {
+          quad map virtrdf:DefaultQuadMap
+          {
+            graph <http://taxref.mnhn.fr/lod/graph/classes/13.0>
+            {
+              ?s1 <http://www.w3.org/2000/01/rdf-schema#label> ?o1 .
+              ?o1 bif:contains ' ( ${stringToSearch} ) '
+		    option ( score ?sc ) .
+	    # ?s1 <http://taxref.mnhn.fr/lod/property/vernacularName> ?vernac .
+	    # ?vernac bif:contains ' ( ${stringToSearch} ) '
+	    # option ( score ?sc2 ) .
+            }
+           }
+         }
+       order by desc ( ?sc * 3e-1 + sql:rnk_scale ( <LONG::IRI_RANK> ( ?s1 ) ) ) limit 20 offset 0
+   }` // end Template literal
+   ) // end encodeURIComponent
+    // + "&format=application/sparql-results+json&timeout=0" // should work , but Virtuoso minor bug!
+    + "&format=application/sparql-results%2Bjson&timeout=0"
+	,
         dataType: "json" ,
         timeout: 30000
     }).done( function (ajaxResponse) {
