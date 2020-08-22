f828c0755349fe769aeef648108e17e227497866
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-08-22 12:48:46 +0200

    Titanium2RDF scala program: apply to given JSON URL given JSON-LD @context URL

---
+++ scala/jsonld/src/main/scala/titaniumJena/Titanium2RDF.scala
@@ -0,0 +1,32 @@
+package titaniumJena
+
+import com.apicatalog.jsonld._
+import com.apicatalog.jsonld.api._
+import com.apicatalog.rdf._
+
+import scala.collection.JavaConverters._
+import org.apache.jena.query.DatasetFactory
+import java.io.ByteArrayOutputStream
+import org.apache.jena.riot.RDFDataMgr
+
+/** apply to given JSON URL given JSON-LD @context URL */
+object Titanium2RDF extends App {
+
+  def jsonLDtoRDF3(uri: String, context: String): RdfDataset = {
+    val options = new JsonLdOptions()
+    options.setExpandContext(context)
+    JsonLd.toRdf(uri).options(options).get
+  }
+
+  val jsonldInput = args(0)
+  val jsonldContext = args(1)
+  val ds = jsonLDtoRDF3(jsonldInput, jsonldContext)
+
+  // cf class Json2RDFServiceApp
+  val dataset = DatasetFactory.create()
+  val datasetGraph = dataset.asDatasetGraph()
+  Titanium2Jena.populateDataset(ds, datasetGraph)
+  println(s"json2rdf:  after populateDataset  size ${datasetGraph.size()}")
+  val outputStream = new ByteArrayOutputStream
+  RDFDataMgr.write(System.out, dataset.getDefaultModel, org.apache.jena.riot.RDFFormat.TURTLE)
+}
