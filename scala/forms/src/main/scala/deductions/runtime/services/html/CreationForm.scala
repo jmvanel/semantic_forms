28b1b8d5ea766a70c83253fea07738f7ca16dc87
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-09-29 14:39:17 +0200

    /create : scroll to form: this is quicker on a smartphone
    NOTE this interfers with scrollnav

--- scala/forms/src/main/scala/deductions/runtime/services/html/CreationForm.scala
+++ scala/forms/src/main/scala/deductions/runtime/services/html/CreationForm.scala
@@ -61,36 +61,18 @@ with PrefillFormDefaults[Rdf, DATASET]{
     ) . flatten
 
     Success(
-      editingHeaders
-        ++
-        rawForm)
+      editingHeaders ++
+        rawForm ++
+        focusOnForm)
   }
-
-
-//  /** make raw Data for a form for instance creation; transactions Inside */
-//  def createData(classUri: String,
-//                 formSpecURI: String = "",
-//                 request: HTTPrequest
-//                 ) : FormSyntax = {
-//    val classURI = URI(classUri)
-//    retrieveURIBody(classURI, dataset, request, transactionsInside = true)
-//    implicit val lang = request.getLanguage()
-//    implicit val graph: Rdf#Graph = allNamedGraph
-//    val form = createFormFromClass(classURI, formSpecURI, request)
-//    form
-//  }
-//
-//  def createDataAsJSON(classUri: String,
-//                       formSpecURI: String = "",
-////                       graphURI: String = "",
-//                       request: HTTPrequest ) = {
-//    val formSyntax =
-////      rdfStore.rw( dataset, {
-//      createData(classUri, formSpecURI, request)
-////    }) . get
-//    formSyntax2JSONString(formSyntax)
-//  }
-
+  def focusOnForm : NodeSeq = {
+    <script>{
+      xml.Unparsed("""
+        document.getElementById("form").scrollIntoView()
+        console.log("scrolled to form")
+      """)}
+    </script>
+  }
 
   /** make form Header about Editing: "CREATING <name of object>" */
   def makeEditingHeader(classUri: String, lang: String,
@@ -109,43 +91,4 @@ with PrefillFormDefaults[Rdf, DATASET]{
       <p>Problem occured when creating an XHTML input form from a class URI.</p>)
   }
 
-//  private def reallyPrefillProperty(prop: Rdf#URI): Boolean = {
-//    val noPrefillProperties: List[Rdf#URI] =
-//      List( geo("lat"), geo("long"), geo("alt"),
-//            URI("http://deductions.github.io/nature_observation.owl.ttl#taxon") )
-//    println ( s"reallyPrefillProperty: <$prop> ${! noPrefillProperties.contains(prop)}")
-//    ! noPrefillProperties.contains(prop)
-//  }
-//
-//  /** create Prefilled input Form, from the Referer URL */
-//  def createPrefillForm(form: FormSyntax, request: HTTPrequest) : FormSyntax = {
-//    import request._
-//    val referer = getHTTPheaderValue("Referer") getOrElse("")
-//    val referenceSubjectURI = URLDecoder.decode( substringAfter( referer, config.hrefDisplayPrefix() ), "UTF-8")
-//    // Referer example: http://localhost:9000/display?displayuri=http%3A%2F%2F172.17.0.1%3A9000%2Fldp%2FHerv%C3%A9_Mureau
-//    logger.debug(s""">>>> createPrefillForm: referenceSubjectURI $referenceSubjectURI
-//        request.path ${request.path}""")
-//    if( referenceSubjectURI != "" &&
-//        path == "/create" &&
-//        getHTTPparameterValue("prefill").getOrElse("").trim() != "no" )
-//    wrapInReadTransaction {
-//      val newTriples = duplicateTree(makeUri(referenceSubjectURI), form.subject, allNamedGraph) . toList
-////      println ( s"createPrefillForm: newTriples \n${newTriples.mkString("\t\n")}")
-////      println ( s"createPrefillForm: form.fields.size ${form.fields.size} :::: \n${form.fields.mkString("\n")}")
-//      val newFfields = for (field <- form.fields) yield {
-////        println ( s"createPrefillForm: form field ${field}")
-//        val found = newTriples.find(triple => triple.predicate == makeURI(field.property))
-////        println ( s"createPrefillForm: found $found")
-//        val f = found match {
-//          case Some(t) if( reallyPrefillProperty(t.predicate) ) => field.copyEntry(value = t.objectt)
-//          case _ => field
-//        }
-////        println ( s"createPrefillForm: newFfield $f")
-//        f
-//      }
-////      println ( s"createPrefillForm: 3 form.fields.size ${form.fields.size} :::: ${form.fields.mkString("\n")}")
-//      form.fields = newFfields
-//    }
-//    form
-//  }
 }
