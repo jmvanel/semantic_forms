6af45d064f21e0813dc58b1a157886a6066e2dcb
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-12-19 19:20:43 +0100

    Forgot titaniumJena/JsonUtils.scala !

---
+++ scala/jsonld/src/main/scala/titaniumJena/JsonUtils.scala
@@ -0,0 +1,23 @@
+package titaniumJena
+
+import java.util.HashMap
+import jakarta.json.stream.JsonGenerator
+import java.io.StringWriter
+import jakarta.json.Json
+import jakarta.json.JsonObject
+
+trait JsonUtils {
+  def prettyPrintJSON(obj: JsonObject) = {
+    val properties =
+      new HashMap[String, Any](1)
+    //				new HashMap<String,*>(1);
+    properties.put(JsonGenerator.PRETTY_PRINTING, true)
+    val sw = new StringWriter()
+    val writerFactory = Json.createWriterFactory(properties);
+    val jsonWriter = writerFactory.createWriter(sw)
+    jsonWriter.writeObject(obj)
+    jsonWriter.close();
+    //				System.out.println(
+    sw.toString()
+  }
+}
