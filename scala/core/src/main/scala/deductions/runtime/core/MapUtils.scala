f4beb308285bf94cbd3aba235f79fd1c47c1d26c
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2021-11-14 20:50:09 +0100

    FIX trouble with Scalaz in http://localhost:9000/create?prefill=no&uri=http%3A%2F%2Frdfs.org%2Fsioc%2Fns%23Post

--- scala/core/src/main/scala/deductions/runtime/core/MapUtils.scala
+++ scala/core/src/main/scala/deductions/runtime/core/MapUtils.scala
@@ -1,14 +1,19 @@
 package deductions.runtime.core
 
-import scalaz.Scalaz._
+// import scalaz.Scalaz._
+// import scalaz._
+// import Scalaz._
 
 trait MapUtils {
 
   protected def getFirstNonEmptyInMap(
     map: Map[String, Seq[String]],
     uri: String): String = {
+    // println(s"getFirstNonEmptyInMap map ${map.mkString(", ")}")
     val uriArgs = map.getOrElse(uri, Seq())
-    uriArgs.find { uri => uri  =/=  "" }.getOrElse("") . trim()
+    // println(s"getFirstNonEmptyInMap uriArgs ${uriArgs.mkString(", ")}")
+    // uriArgs.find { u => ( u =/= "" ) }.getOrElse("") . trim()
+    uriArgs.find { u => ( u != "" ) }.getOrElse("") . trim()
   }
 
 }
